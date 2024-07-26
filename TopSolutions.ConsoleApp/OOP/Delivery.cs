f0508adec2b85253a3b9725de8cb5b9f64a9ceb0
Roy Chang <roy.chang@civica.com.au>
2024-07-26 22:29:23 +1000

    Sample interface usage

---
+++ TopSolutions.ConsoleApp/OOP/Delivery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.OOP
+{
+    public class Delivery
+    {
+        public Delivery() 
+        {
+            SaveFile savedFile = new SaveFile();
+            ISaveable saved = savedFile;
+            saved.Save();
+        
+        }
+        
+    }
+}
