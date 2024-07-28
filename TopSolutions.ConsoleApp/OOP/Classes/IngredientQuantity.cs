21ee593a4c0ceb88b28b3a3796c3bdf93472bedf
Roy Chang <roychang@live.com.au>
2024-07-28 20:04:06 +1000

    Added ingredient quantity

---
+++ TopSolutions.ConsoleApp/OOP/Classes/IngredientQuantity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.OOP.Classes
+{
+    public class IngredientQuantity
+    {
+        public IngredientQuantity() { }
+
+        public string Name { get; set; }    
+        public string Description { get; set; }
+
+    }
+}
