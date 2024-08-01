90ed447a03e3cfb257004645b78a2a6790001de2
Roy Chang <roychang@live.com.au>
2024-08-01 15:41:57 +1000

    Executable test sample for factory pattern

--- TopSolutions.ConsoleApp/Patterns/Factory/Simple/MainCook.cs
+++ TopSolutions.ConsoleApp/Patterns/Factory/Simple/MainCook.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TopSolutions.ConsoleApp.Patterns.Factory.Simple.FactoryClass;
+﻿using TopSolutions.ConsoleApp.Patterns.Factory.Simple.FactoryClass;
 
 namespace TopSolutions.ConsoleApp.Patterns.Factory.Simple
 {
@@ -15,8 +10,8 @@ namespace TopSolutions.ConsoleApp.Patterns.Factory.Simple
 
             if (cook != null) 
             {
-                Console.WriteLine("Welcome statement " + cook.CookFood);
-                Console.WriteLine("Ingredients" + cook.GetIngredients);
+                cook.CookFood();
+                cook.ServerFood();
             }
 
         }
--- TopSolutions.ConsoleApp/Program.cs
+++ TopSolutions.ConsoleApp/Program.cs
@@ -1,17 +1,17 @@
-﻿using TopSolutions.ConsoleApp.Samples.QuickTest;
+﻿//using TopSolutions.ConsoleApp.Samples.QuickTest;
 
-//DateTime.MinValue.ToString(new CultureInfo("en-US"));
+////DateTime.MinValue.ToString(new CultureInfo("en-US"));
 
-var dateStr = DateTime.Today.AddDays(20);//"2023-22-24";
+//var dateStr = DateTime.Today.AddDays(20);//"2023-22-24";
 
-//var dateStr2 = StaticClassTest.GetDateStringValue(dateStr);
-//Console.WriteLine(dateStr2);
+////var dateStr2 = StaticClassTest.GetDateStringValue(dateStr);
+////Console.WriteLine(dateStr2);
 
-var strDate = "11/12/2023 12:00:00 AM";
-var convertToDateTime0AM = StaticClassTest.ConvertToDateTime0AM(StaticClassTest.GetDateTimeValue(strDate));
+//var strDate = "11/12/2023 12:00:00 AM";
+//var convertToDateTime0AM = StaticClassTest.ConvertToDateTime0AM(StaticClassTest.GetDateTimeValue(strDate));
 
-var getDateTimeValue = StaticClassTest.GetDateTimeValue(strDate);
-var getDateTimeValueWithExact = StaticClassTest.GetDateTimeValueWithExact(strDate, "MM/dd/yyyy", true);
+//var getDateTimeValue = StaticClassTest.GetDateTimeValue(strDate);
+//var getDateTimeValueWithExact = StaticClassTest.GetDateTimeValueWithExact(strDate, "MM/dd/yyyy", true);
 
-var date3 = StaticClassTest.GetStringValue(strDate);
-Console.WriteLine(date3);
+//var date3 = StaticClassTest.GetStringValue(strDate);
+//Console.WriteLine(date3);
