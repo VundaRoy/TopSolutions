77a4a51143fb11fbfe4eee03b4046502a8aefc00
Roy Chang <roychang@live.com.au>
2024-08-01 15:55:12 +1000

    Simple test of factory sample

---
+++ TopConsoleNunit.Test/Patterns/Factory/CookTest.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using TopSolutions.ConsoleApp.OOP.Classes;
+using TopSolutions.ConsoleApp.OOP.Interface;
+using TopSolutions.ConsoleApp.Patterns.Factory.Simple.Contract;
+
+namespace TopConsoleNunit.Test.Patterns.Factory
+{
+    public class CookTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AsianCookTest()
+        {
+            var serviceMock = new Mock<ICook>();
+            
+            
+            serviceMock.Setup(n => n.CookFood());
+
+            Assert.IsFalse(serviceMock.Name.Equals(null));
+        }
+    }
+}
