126d373ae793d8a6402ba44b77255315878d8d31
Roy Chang <roychang@live.com.au>
2024-08-03 17:30:55 +1000

    Basic singleton example

---
+++ TopSolutions.ConsoleApp/Patterns/Singleton/Basic/OneWay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Singleton.Basic
+{
+    public class OneWay
+    {
+        private static OneWay _oneTime;
+        private OneWay() { }
+        public static OneWay OneTime
+        {
+
+            get
+            {
+                if (_oneTime == null)
+                    _oneTime = new OneWay();
+                return _oneTime;
+
+            }
+
+        }
+        public void DoOnewayRun()
+        {
+            Console.WriteLine("Faye runaway");
+        }
+    }
+
+
+}
---
+++ TopSolutions.ConsoleApp/Patterns/Singleton/Basic/SingleTonMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Singleton.Basic
+{
+    public class SingleTonMain
+    {
+      public static void Main() 
+      {
+            OneWay one = OneWay.OneTime;
+            one.DoOnewayRun();
+      }
+
+    }
+}
