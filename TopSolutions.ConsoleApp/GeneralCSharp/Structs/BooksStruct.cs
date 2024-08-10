57b29f1d5c9187761d7502ceb75fc091eb011857
Roy Chang <roychang@live.com.au>
2024-08-10 17:55:01 +1000

    struct sample

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Structs/BooksStruct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Structs
+{
+    struct BooksStruct
+    {        
+        private string title;
+        private string description;
+        private string author;
+        private string subject;
+
+        public void getBookInfo(string t, string d, string a, string s)
+        {
+            title = t.Trim();
+            description = d.Trim();
+            author = a.Trim();
+            subject = s.Trim();
+        }
+        public void displayBook()
+        {
+            Console.WriteLine("Title : {0}", title);
+            Console.WriteLine("Author : {0}", author);
+            Console.WriteLine("Subject : {0}", subject);
+            Console.WriteLine("Description :{0}", description);
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Structs/TestStructure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Structs
+{
+    public class TestStructure
+    {
+        public static void Main(string[] args)
+        {
+            BooksStruct book1 = new BooksStruct();
+            BooksStruct book2 = new BooksStruct();
+
+            book1.getBookInfo("French English Phrases", "A full on language book", "Harrap Editors", "Language learning");
+            book2.getBookInfo("Structural Pattern", "Go4 typical books for engineers", "Keith Reigh", "Software engineering");
+
+            book1.displayBook();
+            book2.displayBook();
+
+            Console.ReadKey();
+
+        }
+    }
+}
