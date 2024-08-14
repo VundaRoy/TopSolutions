d3d6caeb18c63630241f7d9b57d22f717ef8a00c
Roy Chang <roychang@live.com.au>
2024-08-14 18:06:18 +1000

    Hashtable sample

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/HashTableSample/MainHashTable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.HashTableSample
+{
+    public class MainHashTable
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("UserId", 1001);
+            hashtable.Add("Name", "Teriko");
+            hashtable.Add("Salary", 28000);
+            hashtable.Add("Location", "Samabula");
+            hashtable.Add("Email", "aga@tel.com.fj");
+            Console.WriteLine("Printing list");
+            foreach (object key in hashtable) 
+            { 
+                Console.WriteLine(key + " : " + hashtable[key]);
+            }
+            Console.WriteLine("Location : " + hashtable["Location"]);
+            Console.WriteLine("\n Printing using keys ");
+        }
+    }
+}
