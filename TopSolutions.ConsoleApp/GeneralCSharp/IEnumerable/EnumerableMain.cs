3f5ef99927a7a88352205098d91f9b7b14d92ba4
Roy Chang <roychang@live.com.au>
2024-08-15 20:24:39 +1000

    IEnumerator demo and IEnumerable

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/IEnumerable/EnumerableMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.IEnumerable
+{
+    public class EnumerableMain
+    {
+        static void Main(string[] args)
+        {
+            List<string> Month = new List<string>();
+            Month.Add("January");
+            Month.Add("February");
+            Month.Add("March");
+            Month.Add("April");
+            Month.Add("May");
+            Month.Add("June");
+            Month.Add("July");
+            Month.Add("August");
+            Month.Add("September");
+            Month.Add("October");
+            Month.Add("November");
+            Month.Add("December");
+            //create IEnumerable of string
+            IEnumerable<string> iEnumerableOfString = (IEnumerable<string>)Month;
+            foreach (string AllMonths in iEnumerableOfString)
+            {
+                Console.WriteLine(AllMonths);
+            }
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/GeneralCSharp/IEnumerator/EnumeratorMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.IEnumerator
+{
+    public class EnumeratorMain
+    {
+        static void iEnumeratorMethodOne(IEnumerator<string> i)
+        {
+            while (i.MoveNext())
+            {
+                Console.WriteLine(i.Current);
+
+                if (i.Current == "Shravana")
+                {
+                    iEnumeratorMethodTwo(i);
+                }
+            }
+        }
+        static void iEnumeratorMethodTwo(IEnumerator<string> i)
+        {
+            while (i.MoveNext())
+            {
+                Console.WriteLine(i.Current);
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<string> Month = new List<string>();
+            Month.Add("Chaitra");
+            Month.Add("Vaishakka");
+            Month.Add("Jyaishta");
+            Month.Add("Ashadha");
+            Month.Add("Shravana");
+            Month.Add("Kartika");
+            Month.Add("Margashira");
+            Month.Add("Pausha");
+            Month.Add("Magha");
+            Month.Add("Phalguna");
+            //Create IEnumerator of string.
+
+            IEnumerator<string> iEnumeratorOfString = Month.GetEnumerator();//to convert list into IEnumerator we can invoke the GetEnumerator method
+
+            while (iEnumeratorOfString.MoveNext())
+            {
+                Console.WriteLine(iEnumeratorOfString.Current);
+            }
+             iEnumeratorOfString = Month.GetEnumerator();
+            iEnumeratorMethodOne(iEnumeratorOfString);
+        }
+    }
+}
