46f827ba729d83ad14fc60aeb69eee2375b773c0
Roy Chang <roychang@live.com.au>
2024-08-19 21:59:20 +1000

    Func with lambdas

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Func/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Func
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+        public int Divide(int a, int b) => a / b;
+    }
+}
---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Func/FuncMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Func
+{
+    public class FuncMain
+    {
+        static void Main(string[] args)
+        {
+            var funcCalculator = new Calculator();
+
+            Func<int, int, int> add = funcCalculator.Add;
+            Func<int, int, int> subtract = funcCalculator.Subtract;
+            Func<int, int, int> multiply = funcCalculator.Multiply;
+            Func<int, int, int> divide = funcCalculator.Divide;
+            Console.WriteLine($"Addition result: {add(4, 2)}");
+            Console.WriteLine($"Subtraction result: {subtract(4, 2)}");
+
+            //Func with multiple additions
+            Func<int, int, int, int> addTwoNumbers = (x, y, z) => x + y + z ;
+            Console.WriteLine(addTwoNumbers(4, 8, 9));
+            //Func with test of equality
+            Func<int, int, bool> areEqual = (x, y) => x == y;
+            Console.WriteLine(areEqual(2000, 3000));
+
+
+        }
+    }
+}
