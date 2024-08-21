f4ac2237ff4469156303fa1db52815473fcdbf51
Roy Chang <roychang@live.com.au>
2024-08-21 21:10:28 +1000

    Add new employee for example

--- TopSolutions.ConsoleApp/SOLID/DIP/Basic/DIPCompliant/DIPMain.cs
+++ TopSolutions.ConsoleApp/SOLID/DIP/Basic/DIPCompliant/DIPMain.cs
@@ -12,10 +12,19 @@ namespace TopSolutions.ConsoleApp.SOLID.DIP.Basic.DIPCompliant
         {
             EmployeeBusinessLogic employeeBusinessLogic = new EmployeeBusinessLogic();
             Employee emp = employeeBusinessLogic.GetEmployeeDetails(1001);
+            PrintEmployeeDetails(emp);
             
-            Console.WriteLine($"ID: {emp.ID}, Name: {emp.Name}, Department: {emp.Department}, Salary: {emp.Salary}");
             employeeBusinessLogic.UpdateEmployee(1001, emp);
+
+            Employee staff01 = new Employee { ID = 200, Department = "OP", Name = "Sentinel", Salary = 500000 };
+            //emp = employeeBusinessLogic.GetEmployeeDetails(200);
+            PrintEmployeeDetails(staff01);
             Console.ReadKey();
+        }
+
+        static void PrintEmployeeDetails(Employee emp) 
+        {
+            Console.WriteLine($"ID: {emp.ID}, Name: {emp.Name}, Department: {emp.Department}, Salary: {emp.Salary}");
         }
     }
 }
