124918f07df6cc70bb761d64cbfa1deea7205791
Roy Chang <roychang@live.com.au>
2024-09-01 21:00:32 +1000

    Debugging and resolving 404 issue with blazor

--- BlazeAwayApp/Components/Pages/EmployeeOverview.razor.cs
+++ BlazeAwayApp/Components/Pages/EmployeeOverview.razor.cs
@@ -6,10 +6,17 @@ namespace BlazeAwayApp.Components.Pages
     public partial class EmployeeOverview
     {
         public List<Employee> Employees { get; set; } = default!;
+        private Employee? _selectedEmployee;
+        private string Title = "Employee overview";
+        private string Description = "employee overview";
         protected async override Task OnInitializedAsync()
         {
             //await Task.Delay(2000);
             Employees = MockDataService.Employees;
+        }
+        public void ShowQuickViewPopup(Employee selectedEmployee)
+        {
+            _selectedEmployee = selectedEmployee;
         }
     }
 }
