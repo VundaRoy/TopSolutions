272355164150370e09fad5423b8c99d89c652857
Roy Chang <roychang@live.com.au>
2024-09-04 21:38:27 +1000

    Bridge pattern

---
+++ TopSolutions.ConsoleApp/Patterns/Bridge/Simple/AbstractRemoteControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Bridge.Simple
+{
+    public abstract class AbstractRemoteControl
+    {
+        protected ILEDTV ledTv;
+        public abstract void SwitchOn();
+        public abstract void SwitchOff();
+        public abstract void SetChannel(int channelNumber);
+    }
+}
