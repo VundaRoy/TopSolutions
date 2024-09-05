a8298ab6a7ad542d0769824874d1c6f94d2fa24f
Roy Chang <roychang@live.com.au>
2024-09-05 18:20:33 +1000

    Another factory design pattern example

---
+++ TopSolutions.ConsoleApp/Patterns/Factory/FactoryDesign/FactoryDesignMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Factory.FactoryDesign
+{
+    public class FactoryDesignMain
+    {
+        static void Main(string[] args) 
+        {
+            IVegetarianRecipe vegetarianRecipe = VegetarianRecipeFactory.GetRecipe("Sattvic");
+            //validate sattvic recipe ingredients
+
+            List<string> ingredients1 = new() { "dal", "ghee","carrot", "dhaniya","taro" };
+            Console.WriteLine("Ingredients 1 is sattvic ? :" + vegetarianRecipe.GetValidIngredients(ingredients1).ToString());
+           
+        
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/Patterns/Factory/FactoryDesign/VeganRecipe.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Factory.FactoryDesign
+{
+    public class VeganRecipe : IVegetarianRecipe
+    {
+        public void CookIngredients()
+        {
+            Console.WriteLine("Making vegan dish, simple as nature");
+        }
+
+        public bool GetValidIngredients(List<string> listOfIngredients)
+        {
+            foreach (var item in listOfIngredients)
+            {
+                Console.WriteLine(item);
+                //validate for onion and garlic
+                if (item != null)
+                {
+                    if (item.Equals("milk", StringComparison.OrdinalIgnoreCase) || item.Equals("honey", StringComparison.OrdinalIgnoreCase)
+                        || item.Equals("butter", StringComparison.OrdinalIgnoreCase)|| item.Equals("cheese", StringComparison.OrdinalIgnoreCase)
+                        || item.Equals("ghee", StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine("Not vegan compliant");
+                        return false;
+                    }
+                }
+
+            }
+            return true;
+        }
+
+        public void PrepareIngredients()
+        {
+            Console.WriteLine("Making vegan your style");
+        }
+
+        public void PresentDish()
+        {
+            Console.WriteLine("Enjoy your vegan dish");
+        }
+    }
+}
