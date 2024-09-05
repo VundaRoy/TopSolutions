56cf8e3cca78956a4761be1ccb295044568b5b94
Roy Chang <roychang@live.com.au>
2024-09-05 19:35:47 +1000

    Additonal test scenarios for factory design

--- TopSolutions.ConsoleApp/Patterns/Factory/FactoryDesign/FactoryDesignMain.cs
+++ TopSolutions.ConsoleApp/Patterns/Factory/FactoryDesign/FactoryDesignMain.cs
@@ -15,8 +15,13 @@ namespace TopSolutions.ConsoleApp.Patterns.Factory.FactoryDesign
 
             List<string> ingredients1 = new() { "dal", "ghee","carrot", "dhaniya","taro" };
             Console.WriteLine("Ingredients 1 is sattvic ? :" + vegetarianRecipe.GetValidIngredients(ingredients1).ToString());
-           
-        
+            List<string> ingredients2 = new() { "dal", "ghee", "carrot", "dhaniya", "garlic", "tomato", "corn" };
+            Console.WriteLine("Ingredients 2 is sattvic ? :" + vegetarianRecipe.GetValidIngredients(ingredients2).ToString());
+            //validate vegan
+            IVegetarianRecipe vegetarianRecipe1 = VegetarianRecipeFactory.GetRecipe("Vegan");
+            Console.WriteLine("Ingredients 1 is vegan compliant ? " + vegetarianRecipe1.GetValidIngredients(ingredients1).ToString() );
+            vegetarianRecipe.PrepareIngredients();
+            vegetarianRecipe1.PrepareIngredients();
         }
     }
 }
