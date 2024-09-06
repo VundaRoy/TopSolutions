981dd8d62126004a892e7702af55f962467dabba
Roy Chang <roychang@live.com.au>
2024-09-06 16:14:15 +1000

    WPF with MVVM

--- UltraWPFApp/App.xaml.cs
+++ UltraWPFApp/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using UltraWPFApp.ViewModel;
 
 namespace UltraWPFApp
 {
@@ -9,6 +10,14 @@ namespace UltraWPFApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow window = new();
+            UserViewModel VM = new();
+            window.DataContext = VM;
+            window.Show();
+        }
     }
 
 }
