3c2932ccb5f6808f672341c98ba25c650cd27dd8
Roy Chang <roychang@live.com.au>
2024-09-13 22:01:43 +1000

    Tuples and lambdas

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Tuples/TuplesMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Tuples
+{
+    public class TuplesMain
+    {
+        static void Main(string[] args)
+        {
+            var values = new List<double>() { 10.85, 20.6, 30.3, 40.05, 50 };
+            //Store the Result of Calulate Method in a variable of Tuple type 
+            Tuple<int, double> t = Calculate(values);
+            //Access the First value using Item1 and second value using Item2 properties
+            Console.WriteLine($"There are {t.Item1} values and their sum is {t.Item2}");
+            Console.ReadKey();
+
+            static int square(int x) => x * x;
+            Console.WriteLine("square of 7 is " + square(7) +" and square of 9 is " +square(9));
+            static int sum(int x, int y, int z) => x + z + y;
+            Console.WriteLine("Sum of 6,8 and 9 is " + sum(6, 7, 9));
+        }
+        //Declaring the return type as Tuple<int, double>
+        private static Tuple<int, double> Calculate(IEnumerable<double> values)
+        {
+            int count = 0;
+            double sum = 0.0;
+            foreach (var value in values)
+            {
+                count++;
+                sum += value;
+            }
+            //Creating an object of Tuple class by calling the static Create method
+            Tuple<int, double> t = Tuple.Create(count, sum);
+            //Returning the tuple instance
+            return t;
+        }
+    }
+}
