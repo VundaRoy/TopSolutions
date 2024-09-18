b490e10715c4a382d274a43b6866fad063d9113b
Roy Chang <roychang@live.com.au>
2024-09-18 20:06:21 +1000

    Nested classes demo

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Classes/Nested/ContainerClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Classes.Nested
+{
+    public class ContainerClass
+    {
+        public ContainerClass()
+        {
+            Console.WriteLine("I am a container class");
+        }
+        public class InnerClass : ContainerClass
+        {
+            public InnerClass()
+            {
+                Console.WriteLine("I am an inner class");
+            }
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Classes/Nested/DemoClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Classes.Nested
+{
+    public class DemoClassMain : ContainerClass.InnerClass
+    {
+        public DemoClassMain()
+        {
+            Console.WriteLine("I am a Demo class");
+        }
+        public static void Main()
+        {
+            DemoClassMain DC = new DemoClassMain();
+        }
+    }
+}
