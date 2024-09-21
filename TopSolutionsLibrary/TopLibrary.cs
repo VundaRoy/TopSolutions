20d790edb94effbfb7e1c00ec52c21f67ab8a450
Roy Chang <roychang@live.com.au>
2024-09-21 20:44:51 +1000

    Added class library for reflection

---
+++ TopSolutionsLibrary/TopLibrary.cs
@@ -0,0 +1,18 @@
+﻿namespace TopSolutionsLibrary
+{
+    public class TopLibrary
+    {
+        public int X;
+        private int Y;
+        public int P1 { get; set; }
+        private int P2 { get; set; }
+        public void Method1()
+        {
+            Console.WriteLine("Method1 Invoked");
+        }
+        private void Method2()
+        {
+            Console.WriteLine("Method2 Invoked");
+        }
+    }
+}
