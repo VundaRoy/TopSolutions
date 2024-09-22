238c876f13147ff62123cf75bc3546bf67dff6e8
Roy Chang <roychang@live.com.au>
2024-09-22 16:33:14 +1000

    Reflection example with properties

---
+++ SuperbLibrary/SuperbClass.cs
@@ -0,0 +1,18 @@
+﻿namespace SuperbLibrary
+{
+    public class SuberbClass
+    {
+        public int X;
+        private int Y;
+        public int P1 { get; set; }
+        private int P2 { get; set; }
+        public void Method1()
+        {
+            Console.WriteLine("Method1 Invoked");
+        }
+        private void Method2()
+        {
+            Console.WriteLine("Method2 Invoked");
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Reflection/ReflectionMain.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Reflection
+{
+    public class ReflectionMain
+    {
+        static void Main(string[] args)
+        {
+            var MyAssembly = Assembly.LoadFile(@"C:\source\etc\core\TopSolutions\SuperbLibrary\bin\Debug\net6.0\SuperbLibrary.dll");
+                        
+            try
+            {
+                foreach (var type in MyAssembly.GetTypes())
+                {
+                    Console.WriteLine(type.FullName);
+                }
+
+                var MyType = MyAssembly.GetType("SuperbLibrary.SuberbClass");
+                dynamic MyObject = Activator.CreateInstance(MyType);
+                Type parameterType = MyObject.GetType();
+
+                //Get all public fields
+                Console.WriteLine("All public fields");
+                foreach (MemberInfo memberInfo in parameterType.GetFields())
+                {
+                    Console.WriteLine(memberInfo.Name);
+                }
+                //get all public methods
+                Console.WriteLine("All public methods");
+                foreach (MemberInfo memberInfo in parameterType.GetMethods())
+                {
+                    Console.WriteLine(memberInfo.Name);
+                }
+                //get all public properties
+                Console.WriteLine("All public properties");
+                foreach (MemberInfo memberInfo in parameterType.GetProperties())
+                {
+                    Console.WriteLine(memberInfo.Name);
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+}
