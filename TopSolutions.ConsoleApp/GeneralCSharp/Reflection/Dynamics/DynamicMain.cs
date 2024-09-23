107fa156bb1ee96be45da3dee5be612174cd513a
Roy Chang <roychang@live.com.au>
2024-09-23 10:47:39 +1000

    Dynamic conversion at runtime

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Reflection/Dynamics/DynamicMain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Reflection.Dynamics
+{
+    public class DynamicMain
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new()
+            {
+                Name = "Anurag",
+                Branch = "CSE",
+                Roll = 1001
+            };
+
+            // Student type to dynamic conversion
+            dynamic dynamicStudent = student1;
+            Console.WriteLine($"Name = {dynamicStudent.Name}");
+            Console.WriteLine($"Branch = {dynamicStudent.Branch}");
+            Console.WriteLine($"Roll = {dynamicStudent.Roll}");
+
+            Console.ReadKey();
+
+            //convert from dynamic to student type
+            dynamic dynamicStudent2 = new Student()
+            {
+                Name = "Anurag",
+                Branch = "CSE",
+                Roll = 1001
+            };
+            // dynamic to Student type conversion
+            Student student2 = dynamicStudent2;
+            Console.WriteLine("Convert from dynamic to student");
+            Console.WriteLine($"Name = {student2.Name}");
+            Console.WriteLine($"Branch = {student2.Branch}");
+            Console.WriteLine($"Roll = {student2.Roll}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Reflection/Dynamics/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Reflection.Dynamics
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Branch { get; set; }
+        public long Roll { get; set; }
+    }
+}
