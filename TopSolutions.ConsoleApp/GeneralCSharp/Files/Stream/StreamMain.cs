1e26e093779093103edae8f02a6980dc5f590189
Roy Chang <roychang@live.com.au>
2024-09-26 19:48:52 +1000

    File Stream create and append

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Files/Stream/StreamMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Files.Stream
+{
+    public class StreamMain
+    {
+        static void Main(string[] args)
+        {
+            string FilePath = @"C:\dev\Text\SampleText.txt";
+            FileStream fileStream = new FileStream(FilePath, FileMode.Append);
+            //Create the byte array which contains the string data to be appended in the File
+            byte[] bytedata = Encoding.Default.GetBytes("C# Is an Object Oriented Programming Language");
+            //count (bytedata.Length): The maximum number of bytes to write.
+            fileStream.Write(bytedata, 0, bytedata.Length);
+            fileStream.Close();
+            Console.WriteLine("File created is in C:\\dev\\Text");
+            Console.ReadKey();
+            
+        }
+    }
+}
