eccc6409ec423dac2d5df254420b7b74827e21a2
Roy Chang <roychang@live.com.au>
2024-09-27 13:39:35 +1000

    Binary writer

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Files/Binary/BinaryMain.cs
@@ -0,0 +1,18 @@
+﻿namespace TopSolutions.ConsoleApp.GeneralCSharp.Files.Binary
+{
+    public class BinaryMain
+    {
+        static void Main(string[] args)
+        {
+            using (BinaryWriter writer = new(File.Open("C:\\dev\\Text\\TheBinary.bin", FileMode.Create)))
+            {
+                writer.Write("0x80234400");
+                writer.Write("Windows error alert");
+                writer.Write(true);
+            }
+            Console.WriteLine("Binary file created ");
+            Console.ReadKey();
+
+        }
+    }
+}
--- TopSolutions.ConsoleApp/GeneralCSharp/Files/Stream/StreamMain.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Files/Stream/StreamMain.cs
@@ -11,7 +11,7 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Files.Stream
         static void Main(string[] args)
         {
             string FilePath = @"C:\dev\Text\SampleText.txt";
-            FileStream fileStream = new FileStream(FilePath, FileMode.Append);
+            FileStream fileStream = new(FilePath, FileMode.Append);
             //Create the byte array which contains the string data to be appended in the File
             byte[] bytedata = Encoding.Default.GetBytes("C# Is an Object Oriented Programming Language");
             //count (bytedata.Length): The maximum number of bytes to write.
@@ -19,8 +19,8 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Files.Stream
             fileStream.Close();
             //Read
             string data;
-            FileStream fileStreamRead = new FileStream(FilePath,FileMode.Open, FileAccess.Read);
-            using (StreamReader sr = new StreamReader(fileStreamRead))
+            FileStream fileStreamRead = new(FilePath,FileMode.Open, FileAccess.Read);
+            using (StreamReader sr = new(fileStreamRead))
             {
                 data = sr.ReadToEnd();
             }
