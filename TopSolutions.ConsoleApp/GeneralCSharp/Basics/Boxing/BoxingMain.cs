871c1a0e5c50cfaf7b8152bef5fe9799a3f281e0
Roy Chang <roychang@live.com.au>
2024-10-04 06:06:52 +1000

    boxing unboxing

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Basics/Boxing/BoxingMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Basics.Boxing
+{
+    public class BoxingMain
+    {
+        static void Main(string[] args)
+        {
+            int x = 100;
+            object y = x;//boxing
+            int z = (int)y;//unboxing
+        }
+    }
+}
