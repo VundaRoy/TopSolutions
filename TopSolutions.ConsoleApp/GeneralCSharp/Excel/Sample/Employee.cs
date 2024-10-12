f5bc1559ff3e796399ec1f2ea48418c2bab6fd15
Roy Chang <roychang@live.com.au>
2024-10-12 22:07:38 +1100

    excel sample I

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Excel/Sample/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Excel.Sample
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public bool IsPermanent { get; set; }
+        public int RegdNo { get; set; }
+        public int Salary { get; set; }
+        public string ProfileURL { get; set; }
+    }
+}
