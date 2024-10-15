e3a207e73f2bc5516d185dd5fcaee8c0e97a0631
Roy Chang <roychang@live.com.au>
2024-10-16 10:33:29 +1100

    Mediator pattern

---
+++ TopSolutions.ConsoleApp/Patterns/Mediator/Basic/IFacebookGroupMediator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Mediator.Basic
+{
+    public interface IFacebookGroupMediator
+    {//This Method is used to send the Message who are registered with the Facebook Group
+        void SendMessage(string msg, User user);
+        //This method is used to register a user with the Facebook Group
+        void RegisterUser(User user);
+    }
+}
