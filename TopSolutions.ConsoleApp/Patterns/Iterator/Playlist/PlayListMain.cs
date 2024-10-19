a1f382a4b414b7a4d8c44f08d9a1cf7b77a1547f
Roy Chang <roychang@live.com.au>
2024-10-19 21:51:01 +1100

    Playlist iterator sample

---
+++ TopSolutions.ConsoleApp/Patterns/Iterator/Playlist/PlayListMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopSolutions.ConsoleApp.Samples.Patterns.Iterator.After.Contracts;
+
+namespace TopSolutions.ConsoleApp.Patterns.Iterator.Playlist
+{
+    public class PlayListMain
+    {
+        static void Main()
+        {
+            var myPlaylist = new Playlist();
+            myPlaylist.AddSong(new Song("Title 1", "Artist 1"));
+            myPlaylist.AddSong(new Song("Title 2", "Artist 2"));
+            myPlaylist.AddSong(new Song("Title 3", "Artist 3"));
+            IIterator<Song> iterator = myPlaylist.CreateIterator();
+            while (iterator.HasNext())
+            {
+                Console.WriteLine(iterator.Next());
+            }
+            Console.ReadLine();
+        }
+    }
+}
