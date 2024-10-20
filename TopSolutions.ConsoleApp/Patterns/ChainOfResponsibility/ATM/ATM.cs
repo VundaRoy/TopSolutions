ada2149cf8c2a54ab1c3d824afcfa74d4ada4b71
Roy Chang <roychang@live.com.au>
2024-10-20 13:02:13 +1100

    Chain of responsibility

---
+++ TopSolutions.ConsoleApp/Patterns/ChainOfResponsibility/ATM/ATM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.ChainOfResponsibility.ATM
+{
+    internal class ATM
+    {
+        private TwoThousandHandler twoThousandHandler = new();
+        private FiveHundredHandler fiveHundredHandler = new();
+        private TwoHundredHandler twoHundredHandler = new();
+        private HundredHandler hundredHandler = new();
+        public ATM()
+        {
+            // Prepare the chain of Handlers
+            // Here, we need to set the next handler of each handler
+            twoThousandHandler.SetNextHandler(fiveHundredHandler);
+            fiveHundredHandler.SetNextHandler(twoHundredHandler);
+            twoHundredHandler.SetNextHandler(hundredHandler);
+        }
+        //The following method handle the request and passes it to the first handler in the chain of responsibility.
+        public void Withdraw(long requestedAmount)
+        {
+            //First check whether the amount is Divisible by 100 or not
+            if (requestedAmount % 100 == 0)
+            {
+                twoThousandHandler.DispatchNote(requestedAmount);
+            }
+            else
+            {
+                Console.WriteLine($"You Enter Invalid Amount: {requestedAmount}");
+            }
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/Patterns/ChainOfResponsibility/ATM/HundredHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.ChainOfResponsibility.ATM
+{
+    public class HundredHandler : Handler
+    {
+        public override void DispatchNote(long requestedAmount)
+        {
+            //First Check the Number of 100 Notes To Be Dispatched
+            long numberofNotesToBeDispatched = requestedAmount / 100;
+            if (numberofNotesToBeDispatched > 0)
+            {
+                if (numberofNotesToBeDispatched > 1)
+                {
+                    Console.WriteLine(numberofNotesToBeDispatched + " Hundred notes are dispatched by HundredHandler");
+                }
+                else
+                {
+                    Console.WriteLine(numberofNotesToBeDispatched + " Hundred note is dispatched by HundredHandler");
+                }
+            }
+            //No Need to Check the Next Handler
+        }
+    }
+}
