0f2d7f5c1b4d73574994a5d412455f0a37b694e9
Roy Chang <roychang@live.com.au>
2024-10-21 11:36:40 +1100

    Additional fifty notes

--- TopSolutions.ConsoleApp/Patterns/ChainOfResponsibility/ATM/ATM.cs
+++ TopSolutions.ConsoleApp/Patterns/ChainOfResponsibility/ATM/ATM.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 
 namespace TopSolutions.ConsoleApp.Patterns.ChainOfResponsibility.ATM
 {
-    internal class ATM
+    // This class managed the sequence in which all the handlers are going to be chained together
+    // This class initiates the request to a ConcreteHandler object on the chain
+
+    public class ATM
     {
         private TwoThousandHandler twoThousandHandler = new();
         private FiveHundredHandler fiveHundredHandler = new();
         private TwoHundredHandler twoHundredHandler = new();
         private HundredHandler hundredHandler = new();
+        private FiftyHandler fiftyHandler = new();
         public ATM()
         {
             // Prepare the chain of Handlers
@@ -19,6 +23,7 @@ namespace TopSolutions.ConsoleApp.Patterns.ChainOfResponsibility.ATM
             twoThousandHandler.SetNextHandler(fiveHundredHandler);
             fiveHundredHandler.SetNextHandler(twoHundredHandler);
             twoHundredHandler.SetNextHandler(hundredHandler);
+            hundredHandler.SetNextHandler(fiftyHandler);
         }
         //The following method handle the request and passes it to the first handler in the chain of responsibility.
         public void Withdraw(long requestedAmount)
--- TopSolutions.ConsoleApp/Patterns/ChainOfResponsibility/ATM/Client.cs
+++ TopSolutions.ConsoleApp/Patterns/ChainOfResponsibility/ATM/Client.cs
@@ -19,6 +19,8 @@ namespace TopSolutions.ConsoleApp.Patterns.ChainOfResponsibility.ATM
             atm.Withdraw(600);
             Console.WriteLine("\nRequested Amount 750");
             atm.Withdraw(750);
+            Console.WriteLine("\nRequested Amount 710");
+            atm.Withdraw(710);
             Console.Read();
         }
     }
