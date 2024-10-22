6ad4e05485c77ec82c525a9dd5b1a0fda7937f46
Roy Chang <roychang@live.com.au>
2024-10-22 17:02:58 +1100

    Class adapter sample

--- TopSolutions.ConsoleApp/Patterns/Adapter/ClassAdapter/ThirdPartyBillingSystem.cs
+++ TopSolutions.ConsoleApp/Patterns/Adapter/ClassAdapter/ThirdPartyBillingSystem.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TopSolutions.ConsoleApp.Patterns.Adapter.Basic
+namespace TopSolutions.ConsoleApp.Patterns.Adapter.ClassAdapter
 {
     //Adaptee
     public class ThirdPartyBillingSystem
