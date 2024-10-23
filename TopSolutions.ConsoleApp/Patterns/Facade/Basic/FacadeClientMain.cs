cc7752d03e86be939ac05012606c727d79d3df8a
Roy Chang <roychang@live.com.au>
2024-10-23 17:45:47 +1100

    Facade sample

---
+++ TopSolutions.ConsoleApp/Patterns/Facade/Basic/FacadeClientMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Facade.Basic
+{
+    public class FacadeClientMain
+    {
+        static void Main(string[] args)
+        {
+            //The Client will use the Facade Interface instead of the Subsystems
+            Order order = new Order();
+            order.PlaceOrder();
+            Console.Read();
+        }
+    }
+}
