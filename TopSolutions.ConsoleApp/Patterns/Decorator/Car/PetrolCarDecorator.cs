e392f489290412dc08523f10e2cc8a05bfa2ca76
Roy Chang <roychang@live.com.au>
2024-10-24 19:51:31 +1100

    Decorator pattern

---
+++ TopSolutions.ConsoleApp/Patterns/Decorator/Car/PetrolCarDecorator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Decorator.Car
+{
+    public class PetrolCarDecorator : CarDecorator
+    {
+        //Pass the existing car object while creating the Instance of PetrolCarDecorator class
+        //Also pass the same existing pizza object to the base class constructor 
+        //i.e. CarDecorator abstract class constructor
+        public PetrolCarDecorator(ICar car) : base(car)
+        {
+        }
+        //Overriding the ManufactureCar method to add Petrol Engine
+        public override ICar ManufactureCar()
+        {
+            //First Call the Concrete Components ManufactureCar Method 
+            car.ManufactureCar();
+            //Then Add a Petrol Engine by calling the AddEngine Method
+            AddEngine(car);
+            return car;
+        }
+        public void AddEngine(ICar car)
+        {
+            if (car is BMWCar BMWCar)
+            {
+                BMWCar.Engine = "Petrol Engine";
+                Console.WriteLine("PetrolCarDecorator added Petrol Engine to the Car : " + car);
+            }
+        }
+    }
+}
