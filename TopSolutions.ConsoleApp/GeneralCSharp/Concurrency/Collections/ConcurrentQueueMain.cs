3675e8e01ac6671e0468f9bbca47bad4cf50e2c7
Roy Chang <roychang@live.com.au>
2024-10-28 19:46:55 +1100

    Async collection queue

--- TopSolutions.ConsoleApp/GeneralCSharp/Concurrency/Collections/ConcurrentQueueMain.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Concurrency/Collections/ConcurrentQueueMain.cs
@@ -10,7 +10,8 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Concurrency.Collections
     {
         static void Main()
         {
-            TestQueue();
+            //TestQueue();
+            TestQueueAsync();
             Console.ReadKey();
         }
         public static void TestQueue()
@@ -26,11 +27,24 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Concurrency.Collections
                 Console.WriteLine($"Order: {mobileOrder}");
             }
         }
+        public static void TestQueueAsync()
+        {
+            var MobileOrders = new Queue<string>();
+            Task t1 = Task.Run(() => GetOrders("Pranaya", MobileOrders));
+            Task t2 = Task.Run(() => GetOrders("Anurag", MobileOrders));
+            Task t3 = Task.Run(() => GetOrders("Srinivas", MobileOrders));
+            Task.WaitAll(t1, t2, t3); //Wait till all the tasks are completed
+
+            foreach (var mobileOrder in MobileOrders)
+            {
+                Console.WriteLine($"Order: {mobileOrder}");
+            }
+        }
         private static void GetOrders(string custName, Queue<string> MobileOrders)
         {
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 100; i++)
             {
-                Thread.Sleep(100);
+                //Thread.Sleep(100);
                 string order = string.Format($"{custName} Needs {i + 3} Mobiles");
                 MobileOrders.Enqueue(order);
             }
