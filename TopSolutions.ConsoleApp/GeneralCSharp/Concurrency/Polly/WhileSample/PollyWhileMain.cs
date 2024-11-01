788b72df83888a893d33b77f6a973e383c61fe91
Roy Chang <roychang@live.com.au>
2024-11-01 11:31:01 +1100

    Polly circuit breaker trial

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Concurrency/Polly/WhileSample/PollyWhileMain.cs
@@ -0,0 +1,58 @@
+﻿using NPOI.OpenXmlFormats.Spreadsheet;
+using Polly;
+using Polly.CircuitBreaker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Concurrency.Polly.WhileSample
+{
+    public class PollyWhileMain
+    {
+        public static void Main(string[] args)
+        {
+            var circuitBreakerPolicy = Policy
+                .Handle<Exception>()
+                .CircuitBreaker(
+                exceptionsAllowedBeforeBreaking: 3,
+                durationOfBreak: TimeSpan.FromSeconds(10),
+                onBreak: (exception, timespan) => {
+                    Console.WriteLine("Circuit broken!");
+                },
+                onReset: () => {
+                    Console.WriteLine("Circuit reset!");
+                },
+                onHalfOpen: () => {
+                    Console.WriteLine("Circuit half-open, next call is a trial.");
+                });
+
+            while (true)
+            {
+                try
+                {
+                    circuitBreakerPolicy.Execute(() =>
+                    {
+                        // Your code that might throw exceptions
+                       // SendRequest();
+                        Console.WriteLine("Request sent successfully.");
+                    });
+                }
+                catch (BrokenCircuitException)
+                {
+                    Console.WriteLine("Circuit is open; waiting before retrying...");
+                     Task.Delay(TimeSpan.FromSeconds(15)); // Wait before retrying
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception: {ex.Message}");
+                }
+            }
+
+
+        }
+
+
+    }
+}
