8c69ac867cbbbe59c25b69daf318fa25476fb0d9
Roy Chang <roychang@live.com.au>
2024-11-02 20:15:21 +1100

    Flyweight circle sample

---
+++ TopSolutions.ConsoleApp/Patterns/Flyweight/Circles/FlyweightCircleMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Flyweight.Circles
+{
+    public class FlyweightCircleMain
+    {
+        static void Main(string[] args)
+        {
+            //Creating Circle Objects with Red Color
+            Console.WriteLine("\n Red color Circles ");
+            for (int i = 0; i < 3; i++)
+            {
+                Circle circle = (Circle)ShapeFactory.GetShape("circle");
+                circle.SetColor("Red");
+                circle.Draw();
+            }
+            //Creating Circle Objects with Green Color
+            Console.WriteLine("\n Green color Circles ");
+            for (int i = 0; i < 3; i++)
+            {
+                Circle circle = (Circle)ShapeFactory.GetShape("circle");
+                circle.SetColor("Green");
+                circle.Draw();
+            }
+            //Creating Circle Objects with Blue Color
+            Console.WriteLine("\n Blue color Circles");
+            for (int i = 0; i < 3; ++i)
+            {
+                Circle circle = (Circle)ShapeFactory.GetShape("circle");
+                circle.SetColor("Green");
+                circle.Draw();
+            }
+            //Creating Circle Objects with Orange Color
+            Console.WriteLine("\n Orange color Circles");
+            for (int i = 0; i < 3; ++i)
+            {
+                Circle circle = (Circle)ShapeFactory.GetShape("circle");
+                circle.SetColor("Orange");
+                circle.Draw();
+            }
+            //Creating Circle Objects with Black Color
+            Console.WriteLine("\n Black color Circles");
+            for (int i = 0; i < 3; ++i)
+            {
+                Circle circle = (Circle)ShapeFactory.GetShape("circle");
+                circle.SetColor("Black");
+                circle.Draw();
+            }
+            Console.ReadKey();
+        }
+    }
+}
