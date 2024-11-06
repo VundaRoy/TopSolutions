bb665ae2b02b6fab57dc23e81114408b26517f55
Roy Chang <roychang@live.com.au>
2024-11-06 22:03:57 +1100

    Testing combo for factory design

--- TopSolutions.ConsoleApp/Patterns/Creational/Factory/FactoryDesign/FactoryDesignMain.cs
+++ TopSolutions.ConsoleApp/Patterns/Creational/Factory/FactoryDesign/FactoryDesignMain.cs
@@ -16,12 +16,17 @@ namespace TopSolutions.ConsoleApp.Patterns.Creational.Factory.FactoryDesign
             List<string> ingredients1 = new() { "dal", "ghee", "carrot", "dhaniya", "taro" };
             Console.WriteLine("Ingredients 1 is sattvic ? :" + vegetarianRecipe.GetValidIngredients(ingredients1).ToString());
             List<string> ingredients2 = new() { "dal", "ghee", "carrot", "dhaniya", "garlic", "tomato", "corn" };
+            List<string> ingredients3 = new() { "dal", "carrot", "dhaniya", "tomato", "corn" };
             Console.WriteLine("Ingredients 2 is sattvic ? :" + vegetarianRecipe.GetValidIngredients(ingredients2).ToString());
             //validate vegan
             IVegetarianRecipe vegetarianRecipe1 = VegetarianRecipeFactory.GetRecipe("Vegan");
             Console.WriteLine("Ingredients 1 is vegan compliant ? " + vegetarianRecipe1.GetValidIngredients(ingredients1).ToString());
             vegetarianRecipe.PrepareIngredients();
             vegetarianRecipe1.PrepareIngredients();
+            //validate both vegan and sattvic
+            IVegetarianRecipe sattvicVegan = VegetarianRecipeFactory.GetRecipe("Vegan");
+            Console.WriteLine("Ingredients 3 is vegan compliant ? " + sattvicVegan.GetValidIngredients(ingredients3).ToString());
+            Console.WriteLine("Ingredients 3 is sattvic compliant ? " + sattvicVegan.GetValidIngredients(ingredients3).ToString());
         }
     }
 }
