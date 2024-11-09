e2b87ba421d9513f24db35b2efa5a5a591556687
Roy Chang <roychang@live.com.au>
2024-11-09 20:58:11 +1100

    Use index.cshtml instead of razor

---
+++ TopSolutions.WebApi/View/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using TopSolutions.WebApi.Model;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TopSolutions.WebApi.View
+{
+    public class IndexModel : PageModel
+    {
+        public List<Staff> StaffList { get; set; } 
+        public void OnGet()
+        {
+            StaffList = new List<Staff>
+            {
+                new Staff { StaffId = 1, UserName = "jdoe", FirstName = "John", LastName = "Doe", Email = "jdoe@example.com" },
+                new Staff { StaffId = 2, UserName = "asmith", FirstName = "Anna", LastName = "Smith", Email = "asmith@example.com" }
+            };
+        }
+    }
+}
