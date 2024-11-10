c68c188ff78d7aaf00aa15626452bda60383a03b
Roy Chang <roychang@live.com.au>
2024-11-10 19:56:55 +1100

    Input and output readline, read readkey

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Basics/Input/UserInput.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Basics.Input
+{
+    public class UserInput
+    {
+        public static void Main(string[] args)
+        {
+            string str; int userInput;
+            Console.WriteLine("Enter a string - ");
+            str = Console.ReadLine();
+            Console.WriteLine($"You entered {str}");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.Write("Input using Read() - ");
+            userInput = Console.Read();
+            Console.WriteLine("Ascii Value = {0}", userInput);
+        }
+    }
+}
