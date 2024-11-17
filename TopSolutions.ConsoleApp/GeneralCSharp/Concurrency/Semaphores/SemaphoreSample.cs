72596e09dece7f3d18e966cfe243ef78f2c45272
Roy Chang <roychang@live.com.au>
2024-11-17 16:09:01 +1100

    Semaphore threading

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Concurrency/Semaphores/SemaphoreSample.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Concurrency.Semaphores
+{
+    public class SemaphoreSample
+    {
+        public static Semaphore semaphore = new Semaphore(2, 3);
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread threadObject = new Thread(DoSomeTask)
+                {
+                    Name = "Thread " + i
+                };
+                threadObject.Start();
+            }
+            Console.ReadKey();
+        }
+        static void DoSomeTask()
+        {
+            Console.WriteLine(Thread.CurrentThread.Name + " Wants to Enter into Critical Section for processing");
+            try
+            {
+                //Blocks the current thread until the current WaitHandle receives a signal.   
+                semaphore.WaitOne();
+                //Decrease the Initial Count Variable by 1
+                Console.WriteLine("Success: " + Thread.CurrentThread.Name + " is Doing its work");
+                Thread.Sleep(5000);
+                Console.WriteLine(Thread.CurrentThread.Name + " Exit.");
+            }
+            finally
+            {
+                //Release() method to release semaphore  
+                //Increase the Initial Count Variable by 1
+                semaphore.Release();
+            }
+        }
+    }
+}
