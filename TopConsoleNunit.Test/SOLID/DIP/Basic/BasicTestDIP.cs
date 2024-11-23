cd05ddbfcbba76bcff3be5c9a729480acf633605
Roy Chang <roychang@live.com.au>
2024-11-23 17:50:01 +1100

    Basic Nunit test

---
+++ TopConsoleNunit.Test/SOLID/DIP/Basic/BasicTestDIP.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopSolutions.ConsoleApp.SOLID.DIP.Basic.DIPCompliant;
+
+namespace TopConsoleNunit.Test.SOLID.DIP.Basic
+{
+    [TestFixture]
+    public class BasicTestDIP
+    {
+        private EmployeeDataAccessLogic _employeeDataAccessLogic;
+        [SetUp]
+        public void Setup()
+        {
+            _employeeDataAccessLogic = new EmployeeDataAccessLogic();
+        }
+
+        [Test]
+        [TestCase(1, "UK", 1000, ExpectedResult = 1500)]
+        [TestCase(2, "Canada", 1000, ExpectedResult = 1200)]
+        [TestCase(3, "USA", 1000, ExpectedResult = 1200)]
+        [TestCase(4, "UAE", 1000, ExpectedResult = 1200)]
+        public double ProcessForeignPay_ShouldReturnCorrectPay(int employeeId, string country, double pay)
+        {
+            // Act
+            var result = _employeeDataAccessLogic.ProcessForeignPay(employeeId, country, pay);
+
+            // Assert
+            return result;
+        }
+    }
+}
