9569b5db74e9525889ae92badf6a79ad6686bdf0
Roy Chang <roychang@live.com.au>
2024-11-30 17:08:29 +1100

    Singleton thread safety

---
+++ TopSolutions.ConsoleApp/Patterns/Creational/Singleton/ThreadSafety/NonThreadSafe/SingletonSafe.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace SingletonDemo
+{
+    public sealed class SingletonSafe
+    {
+        //This variable value will be increment by 1 each time the object of the class is created
+        private static int Counter = 0;
+
+        //This variable is going to store the Singleton Instance
+        private static SingletonSafe Instance = null;
+
+        //The following Static Method is going to return the Singleton Instance
+        public static SingletonSafe GetInstance()
+        {
+            //This is not thread-safe
+            if (Instance == null)
+            {
+                Instance = new SingletonSafe();
+            }
+
+            //Return the Singleton Instance
+            return Instance;
+        }
+
+        private SingletonSafe()
+        {
+            //Each Time the Constructor is called, increment the Counter value by 1
+            Counter++;
+            Console.WriteLine("Counter Value " + Counter.ToString());
+        }
+
+        //The following method can be accessed from outside of the class by using the Singleton Instance
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/Patterns/Creational/Singleton/ThreadSafety/ThreadSafe/MainThreadSafe.cs
@@ -0,0 +1,38 @@
+﻿using SingletonDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Creational.Singleton.ThreadSafety.ThreadSafe
+{
+    public class MainThreadSafe
+    {
+        //Example to Understand Thraed-Safe Problem with Singleton Design Pattern
+        //When use in a Multithreaded Environment
+        static void Main(string[] args)
+        {
+            //The following Code will Invoke both methods Parallely using two different Threads
+            Parallel.Invoke(
+                //Let us Assume PrintTeacherDetails method is Invoked by Thread-1
+                () => PrintTeacherDetails(),
+                //Let us Assume PrintStudentDetails method is Invoked by Thread-2
+                () => PrintStudentDetails()
+                );
+            Console.ReadLine();
+        }
+        private static void PrintTeacherDetails()
+        {
+            //Thread-1 Calling the GetInstance() Method of the Singleton class
+            SingletonSafe fromTeacher = SingletonSafe.GetInstance();
+            fromTeacher.PrintDetails("From Teacher");
+        }
+        private static void PrintStudentDetails()
+        {
+            //At the same time, Thread-2 also Calling the GetInstance() Method of the Singleton Class
+            SingletonSafe fromStudent = SingletonSafe.GetInstance();
+            fromStudent.PrintDetails("From Student");
+        }
+    }
+}
