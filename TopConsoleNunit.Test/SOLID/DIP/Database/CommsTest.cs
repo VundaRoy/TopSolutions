1d8dc3539a31dfc29c79f1d253c463e69f3aa178
Roy Chang <roychang@live.com.au>
2024-12-05 21:11:25 +1100

    unit test DIP for database

---
+++ TopConsoleNunit.Test/SOLID/DIP/Database/CommsTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using TopSolutions.ConsoleApp.SOLID.DIP.DatabaseSample;
+
+namespace TopConsoleNunit.Test.SOLID.DIP.Database
+{
+    [TestFixture]
+    public class CommsTest
+    {
+        private Mock<IDataService> _mockDataService;
+        private Comms _comms;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockDataService = new Mock<IDataService>();
+            _comms = new Comms(_mockDataService.Object);
+        }
+
+        [Test]
+        public void SendComms_ShouldCallSaveMethod()
+        {
+            // Arrange
+            var message = "Test message";
+
+            // Act
+            _comms.SendComms(message);
+
+            // Assert
+            _mockDataService.Verify(ds => ds.Save(message), Times.Once);
+        }
+
+        [Test]
+        public void UnSendMessage_ShouldCallRollBackMethod()
+        {
+            // Arrange
+            var message = "Test message";
+
+            // Act
+            _comms.UnSendMessage(message);
+
+            // Assert
+            _mockDataService.Verify(ds => ds.RollBack(message), Times.Once);
+        }
+    }
+}
---
+++ TopConsoleNunit.Test/SOLID/DIP/Database/DatabaseTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using TopSolutions.ConsoleApp.SOLID.DIP.DatabaseSample;
+
+namespace TopConsoleNunit.Test.SOLID.DIP.Database
+{
+    [TestFixture]
+    public class DatabaseTest
+    {
+        private DatabaseService _databaseService;
+        private StringWriter _stringWriter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _databaseService = new DatabaseService();
+            _stringWriter = new StringWriter();
+            Console.SetOut(_stringWriter);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _stringWriter.Dispose();
+        }
+
+        [Test]
+        public void Save_ShouldOutputCorrectMessage()
+        {
+            // Arrange
+            var message = "Test message";
+
+            // Act
+            _databaseService.Save(message);
+
+            // Assert
+            var output = _stringWriter.ToString().Trim();
+            Assert.AreEqual($"Save the message into the database. {message}", output);
+        }
+
+        [Test]
+        public void RollBack_ShouldOutputCorrectMessage()
+        {
+            // Arrange
+            var message = "Test message";
+
+            // Act
+            _databaseService.RollBack(message);
+
+            // Assert
+            var output = _stringWriter.ToString().Trim();
+            Assert.AreEqual($"Rolling back transaction {message}", output);
+        }
+
+    }
+}
