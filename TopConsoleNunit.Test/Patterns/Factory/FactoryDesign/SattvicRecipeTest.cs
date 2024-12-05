e610e2ab38bc97934546416b940f08eadaccb2b0
Roy Chang <roychang@live.com.au>
2024-12-05 21:21:11 +1100

    Factory design unit test

---
+++ TopConsoleNunit.Test/Patterns/Factory/FactoryDesign/SattvicRecipeTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopSolutions.ConsoleApp.Patterns.Creational.Factory.FactoryDesign;
+
+namespace TopConsoleNunit.Test.Patterns.Factory.FactoryDesign
+{
+    [TestFixture]
+    public class SattvicRecipeTest
+    {
+        private SattvicRecipe _sattvicRecipe;
+        private StringWriter _stringWriter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sattvicRecipe = new SattvicRecipe();
+            _stringWriter = new StringWriter();
+            Console.SetOut(_stringWriter);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _stringWriter.Dispose();
+        }
+
+        [Test]
+        public void GetValidIngredients_ShouldReturnFalse_WhenContainsOnionOrGarlic()
+        {
+            // Arrange
+            var ingredients = new List<string> { "tomato", "onion", "carrot" };
+
+            // Act
+            var result = _sattvicRecipe.GetValidIngredients(ingredients);
+
+            // Assert
+            var output = _stringWriter.ToString();
+            Assert.IsFalse(result);
+            StringAssert.Contains("Not sattvic compliant. Ingredient contains onion", output);
+        }
+
+        [Test]
+        public void GetValidIngredients_ShouldReturnTrue_WhenNoOnionOrGarlic()
+        {
+            // Arrange
+            var ingredients = new List<string> { "tomato", "carrot", "spinach" };
+
+            // Act
+            var result = _sattvicRecipe.GetValidIngredients(ingredients);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void PrepareIngredients_ShouldOutputCorrectMessage()
+        {
+            // Act
+            _sattvicRecipe.PrepareIngredients();
+
+            // Assert
+            var output = _stringWriter.ToString().Trim();
+            Assert.AreEqual("Making sattvic your goal", output);
+        }
+    }
+}
