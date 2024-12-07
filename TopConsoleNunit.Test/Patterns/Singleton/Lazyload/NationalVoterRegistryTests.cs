e1ed85cc069018bb22edb33d14d1d9246f3edd84
Roy Chang <roychang@live.com.au>
2024-12-07 15:11:51 +1100

    Unit test for singleton

---
+++ TopConsoleNunit.Test/Patterns/Singleton/Lazyload/NationalVoterRegistryTests.cs
@@ -0,0 +1,28 @@
+﻿using TopSolutions.ConsoleApp.Patterns.Creational.Singleton.LazyLoad;
+
+namespace TopConsoleNunit.Test.Patterns.Singleton.Lazyload
+{
+    [TestFixture]
+    public class NationalVoterRegistryTests
+    {
+        [Test]
+        public void Register_ShouldCallPrintVoter()
+        {
+            // Arrange
+            var nationalVoterRegistry = new NationalVoterRegistry();
+            var expectedOutput = "Voter is oldham";
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                // Act
+                nationalVoterRegistry.Register();
+
+                // Assert
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(expectedOutput, result);
+            }
+        }
+    }
+}
---
+++ TopConsoleNunit.Test/Patterns/Singleton/Lazyload/RegisterVoterTests.cs
@@ -0,0 +1,29 @@
+﻿using TopSolutions.ConsoleApp.Patterns.Creational.Singleton.LazyLoad;
+
+namespace TopConsoleNunit.Test.Patterns.Singleton.Lazyload
+{
+    [TestFixture]
+    public class RegisterVoterTests
+    {
+        [Test]
+        public void PrintVoter_ShouldPrintCorrectVoterName()
+        {
+            // Arrange
+            var registerVoter = RegisterVoter.Instance;
+            var voterName = "oldham";
+            var expectedOutput = $"Voter is {voterName}";
+
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                // Act
+                registerVoter.PrintVoter(voterName);
+
+                // Assert
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(expectedOutput, result);
+            }
+        }
+    }
+}
