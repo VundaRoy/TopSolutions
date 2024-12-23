026447941e09b7fdd7bad041ae6192ffc79c8e42
Roy Chang <roychang@live.com.au>
2024-12-23 17:13:55 +1100

    cascading exceptions with child parent grandparent calls

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Exceptions/Cascading/CascadingException.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Exceptions.Cascading
+{
+    public class CascadingException
+    {
+        public void FirstCall()
+        {
+            try
+            {
+                SecondCall();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        private void SecondCall()
+        {
+            ThirdCall();
+        }
+        private void ThirdCall()
+        {
+            try
+            {
+                var terminal = Rebooted(null);
+            }
+            catch (Exception ex) 
+            {
+                throw;
+            }
+        }
+        private bool Rebooted(string TerminalId)
+        {
+            if ("0".Equals(TerminalId.Trim()))
+                return false;
+            else
+                return true;
+        }
+        public static void Main(string[] args)
+        {
+            CascadingException ce = new();
+            ce.FirstCall();
+        }
+    }
+}
