01a216eaa724acfa8708c9be6c41bbb333f65ca6
Roy Chang <roychang@live.com.au>
2024-12-26 20:28:32 +1100

    Proxy sample with rwo for employees

---
+++ TopSolutions.ConsoleApp/Patterns/Structural/Proxy/Employment/ISharedFolder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Structural.Proxy.Employment
+{
+    public interface ISharedFolder
+    {
+        void PerformRWOperations();
+    }
+}
