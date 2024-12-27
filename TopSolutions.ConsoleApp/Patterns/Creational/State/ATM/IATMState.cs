9d1d03c08f4f7bbd9504c1fc0a844a4cac54a605
Roy Chang <roychang@live.com.au>
2024-12-27 17:00:54 +1100

    State pattern

---
+++ TopSolutions.ConsoleApp/Patterns/Creational/State/ATM/IATMState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Creational.State.ATM
+{
+    public interface IATMState
+    {
+        void InsertDebitCard();
+        void EjectDebitCard();
+        void EnterPin();
+        void WithdrawMoney();
+    }
+}
---
+++ TopSolutions.ConsoleApp/Patterns/Creational/State/ATM/StateMainAtm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Creational.State.ATM
+{
+    public class StateMainAtm
+    {
+        static void Main(string[] args)
+        {
+            // Initially ATM Machine in DebitCardNotInsertedState
+            ATMMachine atmMachine = new ATMMachine();
+            Console.WriteLine("ATM Machine Current state : "
+                            + atmMachine.AtmMachineState.GetType().Name);
+            Console.WriteLine();
+            atmMachine.EnterPin();
+            atmMachine.WithdrawMoney();
+            atmMachine.EjectDebitCard();
+            atmMachine.InsertDebitCard();
+            Console.WriteLine();
+            // Debit Card has been inserted so the internal state of the ATM Machine
+            // has been changed to DebitCardInsertedState
+            Console.WriteLine("ATM Machine Current state : "
+                            + atmMachine.AtmMachineState.GetType().Name);
+            Console.WriteLine();
+            atmMachine.EnterPin();
+            atmMachine.WithdrawMoney();
+            atmMachine.InsertDebitCard();
+            atmMachine.EjectDebitCard();
+            Console.WriteLine("");
+            // Debit Card has been ejected so the internal state of the ATM Machine
+            // has been changed to DebitCardNotInsertedState
+            Console.WriteLine("ATM Machine Current state : "
+                            + atmMachine.AtmMachineState.GetType().Name);
+            Console.Read();
+        }
+    }
+}
