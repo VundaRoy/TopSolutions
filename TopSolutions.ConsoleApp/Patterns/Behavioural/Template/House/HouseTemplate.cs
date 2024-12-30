ee6ecd382f6b6f92d973161d0ce8a4ffcdb231b0
Roy Chang <roychang@live.com.au>
2024-12-31 10:10:26 +1100

    Template pattern with house example

---
+++ TopSolutions.ConsoleApp/Patterns/Behavioural/Template/House/HouseTemplate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Behavioural.Template.House
+{
+    public abstract class HouseTemplate
+    {
+        // Template Method defines the sequence for building a house
+        public void BuildHouse()
+        {
+            //Define the Steps to Build a House
+            BuildFoundation(); //Step1
+            BuildPillars(); //Step2
+            BuildWalls(); //Step3
+            BuildWindows(); //Step4
+            Console.WriteLine("House is Built");
+        }
+        // Methods to be implemented by subclasses
+        protected abstract void BuildFoundation();
+        protected abstract void BuildPillars();
+        protected abstract void BuildWalls();
+        protected abstract void BuildWindows();
+    }
+}
---
+++ TopSolutions.ConsoleApp/Patterns/Behavioural/Template/House/TemplateHouseMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Behavioural.Template.House
+{
+    public class TemplateHouseMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Build a Concrete House\n");
+            HouseTemplate houseTemplate = new ConcreteHouse();
+            //Call the Template Method to Build the Concrete House
+            houseTemplate.BuildHouse();
+            Console.WriteLine();
+            Console.WriteLine("Build a Wooden House\n");
+            houseTemplate = new WoodenHouse();
+            //Call the Template Method to Build the Wooden House
+            houseTemplate.BuildHouse();
+            Console.Read();
+        }
+    }
+}
