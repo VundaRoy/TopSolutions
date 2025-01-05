59d0ce5f14824f681ee50bae2c96ada89c61464e
Roy Chang <roychang@live.com.au>
2025-01-05 12:09:02 +1100

    delete method for webapi

--- TopSolutions.WebApi/Controllers/CountriesController.cs
+++ TopSolutions.WebApi/Controllers/CountriesController.cs
@@ -7,18 +7,30 @@ namespace TopSolutions.WebApi.Controllers
     [Route("api/[controller]")]
     public class CountriesController : ControllerBase
     {
-        
-        [HttpGet]
-        public IActionResult GetCountries()
-        {
-            List<Country> countryList = new()
-            {
+        static List<Country> countryList = new(){
                 new Country { CountryId = 1, CountryName = "Australia", CountryCode = "AU" },
                 new Country { CountryId = 2, CountryName = "New Zealand", CountryCode = "NZ" },
                 new Country { CountryId = 3, CountryName = "Canada", CountryCode = "CA" },
                 new Country { CountryId = 4, CountryName = "United Kingdom", CountryCode = "UK" }
              };
+       
+        [HttpGet]
+        public IActionResult GetCountries()
+        {            
             return Ok(countryList);
+        }
+        // DELETE: api/Country/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCountry(int id)
+        {
+            var country = countryList.FirstOrDefault(s => s.CountryId == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            countryList.Remove(country);
+            return NoContent();
         }
     }
 }
