66885b6cbacb238867d6ad114ec0bffef69dae13
Roy Chang <roychang@live.com.au>
2025-01-06 20:24:39 +1100

    Timing algorithms

---
+++ TopSolutions.ConsoleApp/Algorithms/Sorting/CompareSorts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Algorithms.Sorting
+{
+    public class CompareSorts
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 30, 41, 5, 6, 7, 8, 9,101,112,1002,111,152,112,101,16,18,11,1522,1599,1255,11,105,114,163,32,56,668,95 };
+            SortingAlgos sortAlgos = new SortingAlgos();
+            var res = sortAlgos.SelectionSort(array);
+            Console.WriteLine($"Elapsed Time for selection sort is {res.Elapsed.TotalSeconds} ms");
+            res = sortAlgos.Bubble(array);
+            Console.WriteLine($"Elapsed Time for bubble sort is { res.Elapsed.TotalSeconds } ms");
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/Algorithms/Sorting/SortingAlgos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Algorithms.Sorting
+{
+    public class SortingAlgos
+    {
+        public Stopwatch Bubble(int[] arr)
+        {
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            stopwatch.Stop();
+            return stopwatch;
+        }
+        public Stopwatch SelectionSort(int[] arr)
+        {
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+            stopwatch.Stop();
+            return stopwatch;
+        }
+    }
+}
