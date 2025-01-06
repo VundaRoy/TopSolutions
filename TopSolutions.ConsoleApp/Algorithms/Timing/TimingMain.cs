23002da02f8d39c8e5beaee0502404be07928dd0
Roy Chang <roychang@live.com.au>
2025-01-06 19:56:46 +1100

    timing function

---
+++ TopSolutions.ConsoleApp/Algorithms/Timing/TimingMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Algorithms.Timing
+{
+    public class TimingMain
+    {
+        public static void Main()
+        {
+            var stopwatch = StopStart(1000);
+
+            Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+
+            stopwatch = StopStart(3000);
+
+            Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+        }
+        private static Stopwatch StopStart(int sleep)
+        {
+            Stopwatch stopwatch = new();
+
+            stopwatch.Start();
+            Thread.Sleep(sleep);
+            stopwatch.Stop();
+            return stopwatch;
+        }
+    }
+}
