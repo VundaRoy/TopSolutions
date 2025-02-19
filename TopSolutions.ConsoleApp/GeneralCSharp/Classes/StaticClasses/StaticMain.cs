077180ec382f8216ceac7b25ce8833701bc4b688
Roy Chang <roychang@live.com.au>
2025-02-19 20:50:18 +1100

    static class

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Classes/StaticClasses/StaticMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Classes.StaticClasses
+{
+    public class StaticMain
+    {
+        public static void Main(string[] args)
+        {
+            var enc = MyStatic.EncryptString("walterZinda", "CASI@");
+            var dec = MyStatic.DecryptString(enc, "CASI@");
+            //var anotherDec = MyStatic.DecryptString(enc, "CASI*9"); will fail
+
+        }
+    }
+}
