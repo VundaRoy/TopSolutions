a48f98d16b03dfc138ffa022053e74bd7bb1c2df
Roy Chang <roychang@live.com.au>
2025-03-10 21:36:33 +1100

    Builder revision

--- TopSolutions.ConsoleApp/Patterns/Creational/Builder/Computer/BuilderComputerMain.cs
+++ TopSolutions.ConsoleApp/Patterns/Creational/Builder/Computer/BuilderComputerMain.cs
@@ -10,11 +10,16 @@ namespace TopSolutions.ConsoleApp.Patterns.Creational.Builder.Computer
     {
         public static void Main()
         {
-            ComputerShop shop = new ComputerShop();
+            ComputerShop shop = new();
             ComputerBuilder builder = new GamingComputerBuilder();
             shop.ConstructComputer(builder);
             Computer computer = builder.GetComputer();
             computer.DisplaySpecifications();
+
+            ComputerBuilder devPC = new DeveloperComputerBuilder();
+            shop.ConstructComputer(devPC);
+            Computer comp2 = devPC.GetComputer();
+            comp2.DisplaySpecifications();
             Console.ReadKey();
         }
     }
---
+++ TopSolutions.ConsoleApp/Patterns/Creational/Builder/Computer/DeveloperComputerBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Creational.Builder.Computer
+{
+    public class DeveloperComputerBuilder : ComputerBuilder
+    {
+        public override void SetCPU()
+        {
+            Computer.CPU = "Pentium 3 max";
+        }
+
+        public override void SetHardDrive()
+        {
+            Computer.HardDrive = "SSD HD 2 TB";
+        }
+
+        public override void SetManufacturer()
+        {
+            Computer.Manufacturer = "Pentium C-Class";
+        }
+
+        public override void SetMicrosoftApps()
+        {
+            Computer.MicrosoftApps = "MS Office 360";
+        }
+
+        public override void SetRAM()
+        {
+            Computer.RAM ="120 GB RAM";
+        }
+    }
+}
