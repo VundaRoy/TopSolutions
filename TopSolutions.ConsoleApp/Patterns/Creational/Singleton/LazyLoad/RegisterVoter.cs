024d911816539eca79a182cfcbc33c3bf3bb8cb6
Roy Chang <roychang@live.com.au>
2025-03-12 20:31:45 +1100

    Singleton lazy loading

--- TopSolutions.ConsoleApp/Patterns/Creational/Singleton/LazyLoad/RegisterVoter.cs
+++ TopSolutions.ConsoleApp/Patterns/Creational/Singleton/LazyLoad/RegisterVoter.cs
@@ -8,18 +8,16 @@ namespace TopSolutions.ConsoleApp.Patterns.Creational.Singleton.LazyLoad
 {
     public class RegisterVoter
     {
-        private static readonly Lazy<RegisterVoter> _lazyInstance = new Lazy<RegisterVoter>(() => new RegisterVoter());
+        private static readonly Lazy<RegisterVoter> _lazyInstance = new(() => new RegisterVoter());
         private string _voterName;
 
         private RegisterVoter()
         {
             _voterName = "New Person";
+            Console.WriteLine("Voting in Progress");
         }
         public static RegisterVoter Instance => _lazyInstance.Value;
 
-        public void PrintVoter(string voterName)
-        {
-            Console.WriteLine("Voter is " + voterName);
-        }
+        public void PrintVoter(string voterName) => Console.WriteLine("Voter is " + voterName);
     }
 }
