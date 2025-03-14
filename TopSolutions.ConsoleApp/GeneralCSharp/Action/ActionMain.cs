904b5cc8d75a1af2a08dd37e44bf666a24c9822e
Roy Chang <roychang@live.com.au>
2025-03-14 20:40:29 +1100

    action sample

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Action/ActionMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Action
+{
+    public class ActionMain
+    {
+        public static string result;
+        public static void Main(string[] args)
+        {
+            Action<string, string> JoinWithUnd = (param1, param2) => result = param1 + " und " + param2;
+            JoinWithUnd("Wiefel", "Jarhren");
+            Console.WriteLine($"JoinWithUnd={result}");
+        }
+    }
+}
