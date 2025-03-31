0f8e61af9f657aa0ef13ab4e89759b9d8855366f
Roy Chang <roychang@live.com.au>
2025-03-31 16:28:05 +1100

    certificate serial number extraction

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Security/Certificate/CertificateExtraction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Security.Certificate
+{
+    public class CertificateExtraction
+    {
+        static void Main()
+        {
+            string pfxFilePath = "path/to/your/certificate.pfx";
+            string pfxPassword = "yourPfxPassword";
+
+            // Load the PFX file
+            X509Certificate2 certificate = new X509Certificate2(pfxFilePath, pfxPassword);
+
+            // Get the serial number
+            string serialNumber = certificate.SerialNumber;
+
+            Console.WriteLine($"Serial Number: {serialNumber}");
+        }
+    }
+}
