d3cb737b5139faae18723a5f03de2e7106810357
Roy Chang <roychang@live.com.au>
2025-04-03 11:04:28 +1100

    Simple sample of replacing file with console app

---
+++ CommandConnect/Console/DotNetC#/Files/EditProgramFiles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandConnect.Console.DotNetC_.Files
+{
+    public class EditProgramFiles
+    {
+        public void ReplaceTextWith(string oldText, string newText, string fileName)
+        {
+            string content = File.ReadAllText(fileName);
+            content = content.Replace(oldText, newText);
+            File.WriteAllText(fileName, content);
+            
+        }
+    }
+}
--- CommandConnect/Program.cs
+++ CommandConnect/Program.cs
@@ -8,8 +8,6 @@ namespace CommandConnect
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-        }
+        
     }
 }
