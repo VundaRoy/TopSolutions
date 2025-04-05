d321b6811c7607ce1ad8aa1229f9c4a73ee4fb85
Roy Chang <roychang@live.com.au>
2025-04-05 19:27:20 +1100

    Specialized sample

---
+++ CommandConnect/ConsoleApps/DotNetC#/Concepts/Specialized/Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandConnect.ConsoleApps.DotNetC_.Concepts.Specialized
+{
+    public class Rectangle
+    {
+        public int Length;
+        public int Breadth;
+        public int Area()
+        {
+            return Length * Breadth;
+        }
+        public int Perimeter()
+        {
+            return 2 * (Length + Breadth);
+        }
+    }
+}
