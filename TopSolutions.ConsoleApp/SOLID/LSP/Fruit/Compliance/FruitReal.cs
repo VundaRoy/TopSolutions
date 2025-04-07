b6b196e79e63bfa9ec95758a157506fb757d4aa4
Roy Chang <roychang@live.com.au>
2025-04-07 19:58:49 +1000

    Liskov sample

---
+++ TopSolutions.ConsoleApp/SOLID/LSP/Fruit/Compliance/FruitReal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace TopSolutions.ConsoleApp.SOLID.LSP.Fruit.Compliance
+{
+    public class FruitReal
+    {
+        static void Main(string[] args)
+        {
+            IFruit fruit = new Orange();
+            Console.WriteLine($"Color of Orange: {fruit.GetColor()}");
+            fruit = new Apple();
+            Console.WriteLine($"Color of Apple: {fruit.GetColor()}");
+            Console.ReadKey();
+            //Apple apple = new Orange(); not allowed
+        }
+    }
+    public interface IFruit
+    {
+        string GetColor();
+    }
+    public class Apple : IFruit
+    {
+        public string GetColor()
+        {
+            return "Red";
+        }
+    }
+    public class Orange : IFruit
+    {
+        public string GetColor()
+        {
+            return "Orange";
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/SOLID/LSP/Fruit/NonCompliance/FruitMix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace TopSolutions.ConsoleApp.SOLID.LSP.Fruit.NonCompliance
+{
+    public class FruitMix
+    {
+        static void Main(string[] args)
+        {
+            Apple apple = new Orange();
+            Console.WriteLine(apple.GetColor());
+        }
+    }
+    public class Apple
+    {
+        public virtual string GetColor()
+        {
+            return "Red";
+        }
+    }
+    public class Orange : Apple
+    {
+        public override string GetColor()
+        {
+            return "Orange";
+        }
+    }
+}
