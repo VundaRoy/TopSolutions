403fa36db2bf756172a15bfe7d17b02187ce47b4
Roy Chang <roychang@live.com.au>
2025-04-09 10:42:17 +1000

    Constructor injection

---
+++ TopSolutions.ConsoleApp/SOLID/DIP/ConstructorInjection/BMW.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.SOLID.DIP.ConstructorInjection
+{
+    public class BMW : ICar
+    {
+        private int _miles = 0;
+
+        public int Run()
+        {
+            return ++_miles;
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/SOLID/DIP/ConstructorInjection/DriverClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.SOLID.DIP.ConstructorInjection
+{
+    public class DriverClient
+    {
+        public static void Main(string[] args)
+        {
+            ICar ford = new Ford();
+            Driver driver = new Driver(ford);
+            driver.RunCar();
+        }
+    }
+}
