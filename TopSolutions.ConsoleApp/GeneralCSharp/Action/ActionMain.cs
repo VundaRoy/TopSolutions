6c18980bc26d9e1aac413e4669b98368365fdd05
Roy Chang <roychang@live.com.au>
2025-04-15 12:05:37 +1000

    Action refactor with static variable

--- TopSolutions.ConsoleApp/GeneralCSharp/Action/ActionMain.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Action/ActionMain.cs
@@ -7,13 +7,19 @@ using System.Threading.Tasks;
 namespace TopSolutions.ConsoleApp.GeneralCSharp.Action
 {
     public class ActionMain
-    {
+    {        
         public static string result;
+        public static Action<string> PleaseGet = (param) => result = "Please get " + param;
         public static void Main(string[] args)
         {
+            PleaseGet("bread");
+            Console.WriteLine($"After plea we get: {result}");
             Action<string, string> JoinWithUnd = (param1, param2) => result = param1 + " und " + param2;
             JoinWithUnd("Wiefel", "Jarhren");
             Console.WriteLine($"JoinWithUnd={result}");
+            Action<string> Append_s = (param1) => result = param1 + "'s";
+            Append_s("Lake Geneva");
+            Console.WriteLine($"Append with s looks like {result}");
         }
     }
 }
