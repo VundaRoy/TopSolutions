b680ccfcf7eda10b9303c73361c36466a4002b49
Roy Chang <roychang@live.com.au>
2025-04-17 14:03:27 +1000

    struct edit

--- TopSolutions.ConsoleApp/GeneralCSharp/Structs/TestStructure.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Structs/TestStructure.cs
@@ -12,12 +12,14 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Structs
         {
             BooksStruct book1 = new();
             BooksStruct book2 = new();
+            BooksStruct cantonese = new();
 
             book1.getBookInfo("French English Phrases", "A full on language book", "Harrap Editors", "Language learning");
             book2.getBookInfo("Structural Pattern", "Go4 typical books for engineers", "Keith Reigh", "Software engineering");
-
+            cantonese.getBookInfo("Streetwise Cantonese", "Learn like a Hongkonger", "Sidney Lau", "Language Learning");
             book1.displayBook();
             book2.displayBook();
+            cantonese.displayBook();
 
             Console.ReadKey();
 
