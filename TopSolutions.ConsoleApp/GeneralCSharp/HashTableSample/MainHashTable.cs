07f12c8e7b903543de072b30d7ba85b5e1370ee5
Roy Chang <roychang@live.com.au>
2025-04-19 21:26:32 +1000

    use of ref and hashtables refactoring

--- TopSolutions.ConsoleApp/GeneralCSharp/HashTableSample/MainHashTable.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/HashTableSample/MainHashTable.cs
@@ -5,23 +5,21 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.HashTableSample
 {
     public class MainHashTable
     {
+        Hashtable hashtable;
+        static Hashtable altHash;
+        public MainHashTable()
+        {
+            hashtable = new Hashtable();
+        }
+        
         static void Main(string[] args)
         {
-            Hashtable hashtable = new Hashtable();
-            hashtable.Add("UserId", 1001);
-            hashtable.Add("Name", "Teriko");
-            hashtable.Add("Salary", 28000);
-            hashtable.Add("Location", "Samabula");
-            hashtable.Add("Email", "aga@tel.com.fj");
-            Console.WriteLine("Printing list");
-            foreach (DictionaryEntry key in hashtable) 
-            { 
-                Console.WriteLine(key.Key + " : " + key.Value);
-            }
-            Console.WriteLine("Location : " + hashtable["Location"]);
+            MainHashTable mainHashTable = new MainHashTable();
+            //Hashtable hashtable = new Hashtable();
+            DoHash(ref mainHashTable);
             Console.WriteLine("\n Printing using keys ");
             //without using Add method
-            Hashtable altHash = new()
+            altHash = new()
             {
                 {1, "Towoomba" },
                 {2, "Windhoek" },
@@ -37,6 +35,21 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.HashTableSample
                 Console.WriteLine("{0} is the city of {1}", element, altHash[element]);
             }
 
+        }
+
+        private static void DoHash(ref MainHashTable mainHashTable)
+        {
+            mainHashTable.hashtable.Add("UserId", 1001);
+            mainHashTable.hashtable.Add("Name", "Teriko");
+            mainHashTable.hashtable.Add("Salary", 28000);
+            mainHashTable.hashtable.Add("Location", "Samabula");
+            mainHashTable.hashtable.Add("Email", "aga@tel.com.fj");
+            Console.WriteLine("Printing list");
+            foreach (DictionaryEntry key in mainHashTable.hashtable)
+            {
+                Console.WriteLine(key.Key + " : " + key.Value);
+            }
+            Console.WriteLine("Location : " + mainHashTable.hashtable["Location"]);
         }
     }
 }
