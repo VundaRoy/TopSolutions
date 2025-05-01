7077abe67e233a5e6aa796f112d9336e5e8d0ec3
Roy Chang <roychang@live.com.au>
2025-05-01 10:09:47 +1000

    Continue statement

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Basics/Loops/Continue/ContinueMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Basics.Loops.Continue
+{
+    public class ContinueMain
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                if (i == 3)
+                {
+                    Console.WriteLine($"The count is {i}, Einstein, press any key to continue");
+                    Console.ReadKey();
+                    continue;
+                }
+                Console.WriteLine($"I : {i}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
