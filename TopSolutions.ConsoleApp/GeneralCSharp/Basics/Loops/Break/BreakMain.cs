1972beafd0ff45a87e8f21e3fbaa26fca32ca569
Roy Chang <roychang@live.com.au>
2025-05-02 11:02:30 +1000

    break sample

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Basics/Loops/Break/BreakMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Basics.Loops.Break
+{
+    public class BreakMain
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine($"I : {i} J : {j}");
+                    if (j > 3)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("Out of inner for-loop");
+            }
+            Console.WriteLine("Out of all loops");
+            Console.ReadKey();
+        }
+    }
+}
