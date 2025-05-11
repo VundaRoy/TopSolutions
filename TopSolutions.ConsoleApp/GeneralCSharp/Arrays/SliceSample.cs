2a5c3436d998a8d4252fc8bdfeaafb5a9b718562
Roy Chang <roychang@live.com.au>
2025-05-11 18:51:38 +1000

    substring and slicing

--- TopSolutions.ConsoleApp/GeneralCSharp/Arrays/SliceSample.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Arrays/SliceSample.cs
@@ -14,11 +14,24 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Arrays
             string sample = "Tamavua";
             Console.WriteLine($"The 2nd value of Tamavua is {GetValueBasedOnNumber(2, sample)}");
             Console.WriteLine($"The 3rd value of Tamavua is {GetValueBasedOnNumber(3, sample)}");
+            Console.WriteLine("enter another string: ");
+            sample = Console.ReadLine();
+            Console.WriteLine("enter start: ");
+            var start = Console.ReadLine();
+            Console.WriteLine("enter end: ");
+            var end = Console.ReadLine();
+            
+            Console.WriteLine($"The range gives {GetSliceRange(Convert.ToInt16(start), Convert.ToInt16(end), sample)}");
         }
         static string GetValueBasedOnNumber(int entry, string arrayString)
         {
             var resp = arrayString[..entry].ToString();
             return resp;
+        }
+        static string GetSliceRange(int start, int end, string arrayString)
+        {
+            if (arrayString.Length < end) { return "Wrong entries for start and end";  }
+            return arrayString.Substring(start, end);
         }
     }
 }
