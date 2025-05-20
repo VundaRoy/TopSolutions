5d4ae858893b02b189448b4a77e5c578de5da452
Roy Chang <roychang@live.com.au>
2025-05-21 09:30:11 +1000

    action sample test

--- TopSolutions.ConsoleApp/GeneralCSharp/Action/ActionMain.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Action/ActionMain.cs
@@ -10,16 +10,24 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Action
     {        
         public static string result;
         public static Action<string> PleaseGet = (param) => result = "Please get " + param;
+        static Action<string, string> JoinWith = (param1, param2) => result = param1 + " und " + param2;
         public static void Main(string[] args)
         {
             PleaseGet("bread");
             Console.WriteLine($"After plea we get: {result}");
-            Action<string, string> JoinWithUnd = (param1, param2) => result = param1 + " und " + param2;
-            JoinWithUnd("Wiefel", "Jarhren");
-            Console.WriteLine($"JoinWithUnd={result}");
+            //Conjuction usage
+            Console.WriteLine($"English join is : {GetJoinWith("me", "you", "and")}");
+            Console.WriteLine($"German join is : {GetJoinWith("ich", "sie", "und")}");
+            Console.WriteLine($"French join is : {GetJoinWith("moi", "toi", "et")}");
+
             Action<string> Append_s = (param1) => result = param1 + "'s";
             Append_s("Lake Geneva");
             Console.WriteLine($"Append with s looks like {result}");
+        }
+        public static string GetJoinWith(string first, string second, string conjunction)
+        {
+            JoinWith = (first, second) => result = first + conjunction + second;
+            return result;
         }
     }
 }
