aa0fc92b6ec66257db220493769406d386b0e266
Roy Chang <roychang@live.com.au>
2025-06-04 14:53:39 +1000

    Refactor DIP and enum

--- TopSolutions.ConsoleApp/SOLID/DIP/Basic/DIPCompliant/DIPMain.cs
+++ TopSolutions.ConsoleApp/SOLID/DIP/Basic/DIPCompliant/DIPMain.cs
@@ -13,27 +13,40 @@ namespace TopSolutions.ConsoleApp.SOLID.DIP.Basic.DIPCompliant
         {
             EmployeeBusinessLogic employeeBusinessLogic = new();
             Employee emp = employeeBusinessLogic.GetEmployeeDetails(1001);
+            Employee staff01 = ProcessEmployee(employeeBusinessLogic, emp);
+            //overseas
+
+            OverseasEmployee(employeeBusinessLogic);
+            employeeBusinessLogic.NotifyEmployee(2002);
+            //create new employee
+            employeeBusinessLogic.CreateNewEmloyee();
+            employeeBusinessLogic.CheckEmployeeVisaStatus(staff01.ID);
+        }
+
+        private static void OverseasEmployee(EmployeeBusinessLogic employeeBusinessLogic)
+        {
+            Console.WriteLine("Employee from UAE is " + employeeBusinessLogic.ValidateOverseasEmployee("UAE"));
+            Console.WriteLine("Employee from UK with pay of $60000 is $" + employeeBusinessLogic.GetOverseasEmployeePay(1001, "UK", 60000.00));
+            Console.WriteLine("Whereas employee from Canada with pay of $60000 is $" + employeeBusinessLogic.GetOverseasEmployeePay(1001, "Canada", 60000.00));
+            //Register a Barbudan
+            employeeBusinessLogic.RegisterStudentWithConsulate(2002, Countries.Barbuda.ToString());
+            //Register a Guadapoupian
+            employeeBusinessLogic.RegisterStudentWithConsulate(2003, Countries.Guadeloupe.ToString());
+        }
+
+        private static Employee ProcessEmployee(EmployeeBusinessLogic employeeBusinessLogic, Employee emp)
+        {
             PrintEmployeeDetails(emp);
-            
+
             employeeBusinessLogic.UpdateEmployee(1001, emp);
             employeeBusinessLogic.NotifyEmployee(1001);
             Employee staff01 = new Employee { ID = 200, Department = "OP", Name = "Sentinel", Salary = 500000 };
             //emp = employeeBusinessLogic.GetEmployeeDetails(200);
             PrintEmployeeDetails(staff01);
             Console.ReadKey();
-            //overseas
-
-            Console.WriteLine("Employee from UAE is " + employeeBusinessLogic.ValidateOverseasEmployee("UAE"));
-            Console.WriteLine("Employee from UK with pay of $60000 is $" + employeeBusinessLogic.GetOverseasEmployeePay(1001, "UK", 60000.00));
-            Console.WriteLine("Whereas employee from Canada with pay of $60000 is $" + employeeBusinessLogic.GetOverseasEmployeePay(1001, "Canada", 60000.00));
-            //Register a Barbudan
-            employeeBusinessLogic.RegisterStudentWithConsulate(2002, Countries.Barbuda.ToString());
-            employeeBusinessLogic.NotifyEmployee(2002);
-            //create new employee
-            employeeBusinessLogic.CreateNewEmloyee();
-            employeeBusinessLogic.CheckEmployeeVisaStatus(staff01.ID);
+            return staff01;
         }
-        
+
         static void PrintEmployeeDetails(Employee emp) 
         {
             Console.WriteLine($"ID: {emp.ID}, Name: {emp.Name}, Department: {emp.Department}, Salary: {emp.Salary}");
@@ -47,7 +60,11 @@ namespace TopSolutions.ConsoleApp.SOLID.DIP.Basic.DIPCompliant
             [Description("Curacao")]
             Curacao = 3,
             [Description("Grenada")]
-            Grenada = 4
+            Grenada = 4,
+            Barbados = 5,
+            Trinidad = 6,
+            Guadeloupe = 7,
+            Grenadines = 8
         }
     }
 }
