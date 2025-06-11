f7a44fe2f83d0fa6f80d3a326c4b2800c8b7aaf4
Roy Chang <roychang@live.com.au>
2025-06-12 09:14:48 +1000

    break and continue combo

--- TopSolutions.ConsoleApp/GeneralCSharp/Basics/Loops/Continue/ContinueLoops.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Basics/Loops/Continue/ContinueLoops.cs
@@ -11,17 +11,23 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Basics.Loops.Continue
         public static void Main(string[] args)
         {
             int i = 0, j = 0;
-            for(i=0;i<5;i++)
+            for (i = 0; i < 5; i++)
             {
                 for (j = 0; j < 5; j++)
                 {
-                    if(j== 2)
+                    if (j == 2)
                     {
-                        Console.WriteLine($"i is {i} and j is {j}");
+                        Console.WriteLine($"Inner loop for i is {i} and j is {j}");
                         Console.WriteLine("continue on");
                         continue;
                     }
+                    else if (j == 4)
+                    {
+                        Console.WriteLine($"j is {j} therefore leaving j loop");
+                        break;
+                    }
                 }
+                Console.WriteLine($"Outer loop for i is {i} and j is {j}");
             }
         }
     }
