a9a2bf1e05a036173127c536576e97d6ae3e0e0d
Roy Chang <roychang@live.com.au>
2025-06-20 10:09:43 +1000

    ISP review

---
+++ TopSolutions.ConsoleApp/SOLID/ISP/Basic/ISPMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.SOLID.ISP.Basic
+{
+    public class ISPMain
+    {
+        public static void Main(string[] args)
+        {
+            ExtendedOffice extendedOffice = new ExtendedOffice();
+            Console.WriteLine(extendedOffice.CheckDispenser(2));
+            Violator violator = new Violator();
+            violator.Submit("killjoy");  // not implemented violation
+
+        }
+    }
+}
