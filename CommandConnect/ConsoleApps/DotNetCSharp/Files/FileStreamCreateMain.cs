8542501427bd276f43856bab190170eae5ee5a7b
Roy Chang <roychang@live.com.au>
2025-06-26 10:14:54 +1000

    reburbish console app

---
+++ CommandConnect/ConsoleApps/DotNetCSharp/Files/FileStreamCreateMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandConnect.ConsoleApps.DotNetCSharp.Files
+{
+    public class FileStreamCreateMain
+    {
+        public static void Main(string[] args)
+        {
+            string FilePath = @"C:\dev\MyFile.txt";
+            FileStream fileStream = new FileStream(FilePath, FileMode.Create);
+            fileStream.Close();
+            Console.WriteLine("File has been created and the path is C:\\dev\\MyFile.txt ");
+        }
+    }
+}
