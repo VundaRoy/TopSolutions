82f257c5021468fd901cbf35df4c19f923893052
Roy Chang <roychang@live.com.au>
2025-06-27 11:27:35 +1000

    Writing file stream - took a while to compile

---
+++ CommandConnect/ConsoleApps/DotNetCSharp/Files/FileStreamWriteMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandConnect.ConsoleApps.DotNetCSharp.Files
+{
+    public class FileStreamWriteMain
+    {
+        public static void Main(string[] args)
+        {
+            string FilePath = @"C:\dev\MyFile.txt";
+            FileStream fileStream = new FileStream(FilePath, FileMode.Append);
+            byte[] byteData = Encoding.Default.GetBytes("Welcome to the c# world");
+            fileStream.Write(byteData, 0, byteData.Length);
+            fileStream.Close();
+            Console.WriteLine("Written to file");
+            Console.ReadKey();
+        }
+    }
+}
