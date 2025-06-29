6155f6fddca895829f2c5c56b6580757d814fb89
Roy Chang <roychang@live.com.au>
2025-06-30 09:17:15 +1000

    Input to file stream via console

---
+++ CommandConnect/ConsoleApps/DotNetCSharp/Files/FileStreamInputWriteMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandConnect.ConsoleApps.DotNetCSharp.Files
+{
+    public class FileStreamInputWriteMain
+    {
+        public static void Main(string[] args)
+        {
+            string FilePath = @"C:\dev\MyFile.txt";
+            Console.WriteLine("Write what you may...");
+
+            string inputData = Console.ReadLine();
+            using(StreamWriter streamWriter = new StreamWriter(FilePath, true))
+            {
+                streamWriter.WriteLine(inputData);
+            }
+            Console.WriteLine("Input data has been written");
+            Console.ReadKey();
+               
+        }
+    }
+}
