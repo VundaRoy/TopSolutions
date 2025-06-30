d3c6eda90a3411af60590bac0e0e5035e5e46f5a
Roy Chang <roychang@live.com.au>
2025-07-01 09:36:03 +1000

    StackAlloc with unsafe

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/StackAlloc/StackAllocMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.StackAlloc
+{
+    public class StackAllocMain
+    {
+        public static void Main(string[] args)
+        {
+            unsafe
+            {
+                int* ptr = stackalloc int[11];
+                for(int i = 0; i < 11; i++)
+                {
+                    ptr[i] = i + 1;
+                }
+                for (int i = 0; i < 11; i++) 
+                { 
+                    Console.WriteLine(ptr[i]);  
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
