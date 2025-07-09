733151b2f624ad70d270569dc424b081400770d5
Roy Chang <roychang@live.com.au>
2025-07-09 10:37:24 +1000

    dispose sample

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Dispose/DisposeMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Dispose
+{
+    public class DisposeMain
+    {
+        public static void Main(string[] args)
+        {
+            using(var resourceHolder = new ResourceHolder())
+            {
+                Console.WriteLine("In the heart of it!");
+            }
+            var anotherResourceHolder = new ResourceHolder();
+            anotherResourceHolder.Dispose();
+            var finalResourceHolder = new ResourceHolder();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Dispose/ResourceHolder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Dispose
+{
+    public class ResourceHolder : IDisposable
+    {
+        private bool _disposed = false;
+        public ResourceHolder() 
+        {
+            Console.WriteLine("Unmanaged resources allocated");
+
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if ((!_disposed))
+            {
+                if (disposing)
+                {
+                    Console.WriteLine("Free other managed objects");
+                }
+                Console.WriteLine("Unmanaged resource released.");
+                _disposed = true;
+            }
+        }
+        ~ResourceHolder() 
+        { 
+            Dispose(false);
+            Console.WriteLine("Finilizer called");
+        }
+    }
+}
