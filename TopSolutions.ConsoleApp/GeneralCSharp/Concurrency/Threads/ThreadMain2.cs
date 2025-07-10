66cb15f81d6b21da23c16b808943822b763a9d62
Roy Chang <roychang@live.com.au>
2025-07-10 11:05:36 +1000

    Non thread pool

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Concurrency/Threads/ThreadMain2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Concurrency.Threads
+{
+    public class ThreadMain2
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i< 10; i++)
+            {
+                Thread thread = new Thread(MyMethod)
+                {
+                    Name = "Thread" + i
+                };
+                thread.Start();
+            }
+            Console.Read();
+        }
+        public static void MyMethod(object obj)
+        {
+            Thread thread = Thread.CurrentThread;
+            string message = $"Background: {thread.IsBackground}, Thread Pool: {thread.IsThreadPoolThread}," +
+                $"Thread ID: {thread.ManagedThreadId}";
+            Console.WriteLine(message);
+        }
+    }
+}
