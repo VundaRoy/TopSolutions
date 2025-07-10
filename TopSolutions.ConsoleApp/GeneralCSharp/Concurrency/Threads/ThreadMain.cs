9f4f1b8d588aa3a4859854f06a1b30ab4824264e
Roy Chang <roychang@live.com.au>
2025-07-10 10:56:40 +1000

    Thread pool

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Concurrency/Threads/ThreadMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Concurrency.Threads
+{
+    public class ThreadMain
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i< 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(MyMethod));
+            }
+            Console.Read();
+        }
+        public static void MyMethod(object obj)
+        {
+            Thread thread = Thread.CurrentThread;
+            string message = $"Background: {thread.IsBackground}, Thread Pool: {thread.IsThreadPoolThread}," +
+                $"Thread ID: {thread.ManagedThreadId}";
+            Console.WriteLine(message);
+        }
+    }
+}
