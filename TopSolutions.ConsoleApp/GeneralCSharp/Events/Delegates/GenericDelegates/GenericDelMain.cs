8a4cf134e364511c88d5415cb9a81be9b62eb36e
Roy Chang <roychang@live.com.au>
2025-07-12 20:00:58 +1000

    Func lambda change

--- TopSolutions.ConsoleApp/GeneralCSharp/Events/Delegates/GenericDelegates/GenericDelMain.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Events/Delegates/GenericDelegates/GenericDelMain.cs
@@ -10,23 +10,24 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Events.Delegates.GenericDelegate
     {
         static void Main(string[] args)
         {
-            Func<int, float, double, double> obj1 = new(AddNumber1);
+            //Func
+            Func<int, float, double, double> obj1 = new(AddIntFloatDoubleReturn);
             double Result = obj1.Invoke(100, 125.45f, 456.789);
             Console.WriteLine($"Adding int, float and double makes : {Result}");
-            Action<int, float, double> obj2 = new(AddNumber2);
+            //Action
+            Action<int, float, double> obj2 = new(AddIntFloatDoubleVoid);
             obj2.Invoke(50, 255.45f, 123.456);
+            //use Predicate
             Predicate<string> obj3 = new(CheckLength);
             bool Status = obj3.Invoke("Pranayama");
             Console.WriteLine("Length is > 5 : " +Status);
             Status = obj3.Invoke("Pran");
             Console.WriteLine("Length is > 5 : " + Status);
             Console.ReadKey();
+        
         }
-        public static double AddNumber1(int no1, float no2, double no3)
-        {
-            return no1 + no2 + no3;
-        }
-        public static void AddNumber2(int no1, float no2, double no3)
+        public static double AddIntFloatDoubleReturn(int no1, float no2, double no3) => no1 + no2 + no3;
+        public static void AddIntFloatDoubleVoid(int no1, float no2, double no3)
         {
             Console.WriteLine(no1 + no2 + no3);
         }
