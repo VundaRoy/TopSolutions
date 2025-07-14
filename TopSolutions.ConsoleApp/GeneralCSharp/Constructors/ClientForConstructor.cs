eff0c623b9ad2614b1a53712bffccf156a1e1dc0
Roy Chang <roychang@live.com.au>
2025-07-14 12:08:07 +1000

    Finalizers and destructors

--- TopSolutions.ConsoleApp/GeneralCSharp/Constructors/ClientForConstructor.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Constructors/ClientForConstructor.cs
@@ -8,13 +8,19 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Constructors
 {
     public class ClientForConstructor 
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            ClassConstructor constructor = new ClassConstructor();
+            ClassConstructor constructor = new();
             constructor.SomeMeth();
             //constructor.Dispose();
+
+            constructor = null; // Eligible for GC
+            GC.Collect(); // Force garbage collection
+            GC.WaitForPendingFinalizers(); // Wait for finalizers to run
+            Thread.Sleep(1000); // give time for finalizer to print
+
         }
 
-        
+
     }
 }
