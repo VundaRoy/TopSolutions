1d1f092943b7daaed37e4f062d124cdc9d3dfba4
Roy Chang <roychang@live.com.au>
2025-07-23 09:58:56 +1000

    refactor slicing

--- TopSolutions.ConsoleApp/GeneralCSharp/Arrays/MoreSlicing.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Arrays/MoreSlicing.cs
@@ -12,20 +12,13 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Arrays
         public static void Main(string[] args)
         {
             int[] arr = { 1, 2, 12, 22, 90, 6, 77, 122, 9, 23, 112 };
-            Console.WriteLine(string.Join(", ", arr));
-            Console.WriteLine("Start slice: ");
-            var start = Console.ReadLine();
-            Console.WriteLine("End slice: ");
-            var end = Console.ReadLine();
-            var res = SliceBetween(Convert.ToInt16(start), Convert.ToInt16(end), arr);
-
+            string? end;
+            //Get slice of array given start and end
+            SliceReturn(arr, out string? start, out end, out int[] res);
+            //Get last 3
             string resultString = string.Join(", ", res);
-            Console.WriteLine($"Sliced between {start} and {end} is " +
-                $"{resultString}");
-            res = LastThreeNumbers(arr);
-            resultString = string.Join(", ", res);
-            Console.WriteLine($"The last 3 numbers are {resultString}");
-            var word = Console.ReadLine();
+            string? word;
+            resultString = LastThreeReturn(arr, start, end, out res, resultString, out word);
             if (word != null && word.Length > 3)
             {
                 Console.WriteLine($"The word {word} between 2 and 4 is {SliceStringBetween(2, 4, word)}");
@@ -35,7 +28,29 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Arrays
             //Skip the nth number
             Console.WriteLine("Enter the nth number to skip");
             var num = Console.ReadLine();
-            Console.WriteLine($"The skipped number of {SkipNthNumber(arr,Convert.ToInt32(num))}");
+            Console.WriteLine($"The skipped number of {SkipNthNumber(arr, Convert.ToInt32(num))}");
+        }
+
+        private static string LastThreeReturn(int[] arr, string? start, string? end, out int[] res, string resultString, out string? word)
+        {
+            Console.WriteLine($"Sliced between {start} and {end} is " +
+                            $"{resultString}");
+            res = LastThreeNumbers(arr);
+            resultString = string.Join(", ", res);
+            Console.WriteLine($"The last 3 numbers are {resultString}");
+            Console.WriteLine("Press enter to continue...");
+            word = Console.ReadLine();
+            return resultString;
+        }
+
+        private static void SliceReturn(int[] arr, out string? start, out string? end, out int[] res)
+        {
+            Console.WriteLine(string.Join(", ", arr));
+            Console.WriteLine("Start slice: ");
+            start = Console.ReadLine();
+            Console.WriteLine("End slice: ");
+            end = Console.ReadLine();
+            res = SliceBetween(Convert.ToInt16(start), Convert.ToInt16(end), arr);
         }
 
         static int[] SliceBetween(int start, int end, int[] arrayInt)
