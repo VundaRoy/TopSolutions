bf0babb0caed89aadc0baca2a19d1c74f3fe20d6
Roy Chang <roychang@live.com.au>
2025-07-29 09:32:37 +1000

    read write property

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Properties/Candidate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Properties
+{
+    public class Candidate
+    {
+        private int _CandidateId, _Age;
+        private string _CandidateName, _PoliticalParty;
+
+        public int CandidateId
+        {
+           set { _CandidateId = value; }
+            get { return _CandidateId; }
+        }
+        public int Age
+        {
+            set { _Age = value; }
+            get { return _Age; }
+        }
+        public string CandidateName
+        {
+            set { _CandidateName = value; }
+            get { return _CandidateName; }
+        }
+        public string PoliticalParty
+        {
+            set { _PoliticalParty = value; }
+            get { return _PoliticalParty; }
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Properties/PropertyMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Properties
+{
+    public class PropertyMain
+    {
+        public static void Main(string[] args)
+        {
+            Candidate candidate = new Candidate
+            {
+                CandidateId = 1,
+                CandidateName = "Forbes",
+                Age = 31,
+                PoliticalParty = "Dem"
+            };
+            Console.WriteLine($"Candidate {candidate.CandidateId} is runner for {candidate.PoliticalParty}");
+
+        }
+    }
+}
