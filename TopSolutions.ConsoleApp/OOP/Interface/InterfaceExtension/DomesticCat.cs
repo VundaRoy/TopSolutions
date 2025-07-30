647f50390e8a7ddb25e901d83a583a323c5c6fe2
Roy Chang <roychang@live.com.au>
2025-07-30 10:49:53 +1000

    Interface extension

---
+++ TopSolutions.ConsoleApp/OOP/Interface/InterfaceExtension/DomesticCat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.OOP.Interface.InterfaceExtension
+{
+    public class DomesticCat : IFelidae
+    {
+        public void BreastFeed()
+        {
+            Console.WriteLine("Female Cats breastfeed kittens");
+        }
+
+        public void ConsumesMeat()
+        {
+            Console.WriteLine("All cats east meat almost exclusively");
+        }
+
+        public void HasRetractileClaws()
+        {
+            Console.WriteLine("All cats have retractile claws and soft fur");
+        }
+        public void Meows()
+        {
+            Console.WriteLine("Cats makes meow sound");
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/OOP/Interface/InterfaceExtension/MammalMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.OOP.Interface.InterfaceExtension
+{
+    public class MammalMain
+    {
+        public static void Main(string[] args)
+        {
+            DomesticCat cat = new();
+            cat.BreastFeed();
+            cat.HasRetractileClaws();
+            cat.ConsumesMeat();
+        }
+    }
+}
