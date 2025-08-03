346539aaa195b97ff577b328bc49dcdf096b9fc6
Roy Chang <roychang@live.com.au>
2025-08-04 09:26:58 +1000

    overloading and switch with ranges

--- TopSolutions.ConsoleApp/OOP/Polymorphism/Overload.cs
+++ TopSolutions.ConsoleApp/OOP/Polymorphism/Overload.cs
@@ -1,4 +1,6 @@
-﻿namespace TopSolutions.ConsoleApp.OOP.Polymorphism
+﻿using NPOI.OpenXmlFormats.Dml.Diagram;
+
+namespace TopSolutions.ConsoleApp.OOP.Polymorphism
 {
     public class Overload
     {
@@ -23,6 +25,23 @@
                 Console.WriteLine("Non-A station selected");
             }
         }
+        public void StationName(int input)
+        {
+            switch(input){
+                case int n when (n >= 1 && n <= 10):
+                    Console.WriteLine("Southern lines");
+                    break;
+                case int n when (n>= 11 && n <= 20):
+                    Console.WriteLine("Northern lines");
+                    break;
+                case int n when (n>= 21 && n <= 30):
+                    Console.WriteLine("Western lines");
+                    break;
+                    default:
+                    Console.WriteLine("Invalid line entered");
+                    break ;
+            }
+        }
 
         public static void Main(string[] args) 
         { 
@@ -30,6 +49,8 @@
             ov.StationName("A");
             var station = ov.StationName('C');
             Console.WriteLine(station);
+            //enter integer
+            ov.StationName(2);
         }
     }
 }
