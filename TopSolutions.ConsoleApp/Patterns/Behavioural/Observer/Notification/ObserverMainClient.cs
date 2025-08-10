3ec5822263196a575a06138db569c0221a737edc
Roy Chang <roychang@live.com.au>
2025-08-10 15:43:57 +1000

    observer refactoring

--- TopSolutions.ConsoleApp/Patterns/Behavioural/Observer/Notification/ObserverMainClient.cs
+++ TopSolutions.ConsoleApp/Patterns/Behavioural/Observer/Notification/ObserverMainClient.cs
@@ -9,18 +9,14 @@ namespace TopSolutions.ConsoleApp.Patterns.Behavioural.Observer.Notification
     public class ObserverMainClient
     {
         static void Main()
-        {
+        {            
+            var springObservers = GetObserversList();
             //Create a Product with Out of Stock Status
-            Subject RedMI = new ("Red MI Mobile", 652.65, "Out Of Stock");
+            Subject RedMI = new("Red MI Mobile", 652.65, "Out Of Stock");
+            AddObserversToSubscription(RedMI, springObservers);
             //User Anurag will be created and the user1 object will be registered to the subject
-            Observer user1 = new("Anurag", "124 Anderi West");
-            user1.AddSubscriber(RedMI);
-            //User Pranaya will be created and the user1 object will be registered to the subject
-            Observer user2 = new ("Pranaya", "23 Atola Ave");
-            user2.AddSubscriber(RedMI);
-            //User Priyanka will be created and the user3 object will be registered to the subject
-            Observer user3 = new ("Priyanka", "3/7 Quaker rd. Tourisma");
-            user3.AddSubscriber(RedMI);
+            AddObservers(RedMI, out Observer user1, out Observer user2, out Observer user3);
+            
             Console.WriteLine("Red MI Mobile current state : " + RedMI.GetAvailability());
             Console.WriteLine();
             user3.RemoveSubscriber(RedMI);
@@ -31,7 +27,41 @@ namespace TopSolutions.ConsoleApp.Patterns.Behavioural.Observer.Notification
             Console.Read();
             Subject OrangeNet = new("Orange mobile", 745.29, "New arrivals");
             user1.AddSubscriber(OrangeNet);
+            user2.AddSubscriber(OrangeNet);
+            AddObserversToSubscription(OrangeNet, springObservers);
             OrangeNet.SetAvailability(" in your local store ");
         }
+
+        private static void AddObserversToSubscription(Subject target, List<Observer> observers)
+        {
+            foreach(Observer observer in observers)
+            {
+                observer.AddSubscriber(target);
+            }
+        }
+
+        private static void AddObservers(Subject RedMI, out Observer user1, out Observer user2, out Observer user3)
+        {
+            user1 = new("Anurag", "124 Anderi West");
+            user1.AddSubscriber(RedMI);
+            //User Pranaya will be created and the user1 object will be registered to the subject
+            user2 = new("Pranaya", "23 Atola Ave");
+            user2.AddSubscriber(RedMI);
+            //User Priyanka will be created and the user3 object will be registered to the subject
+            user3 = new("Priyanka", "3/7 Quaker rd. Tourisma");
+            user3.AddSubscriber(RedMI);
+        }
+        private static List<Observer> GetObserversList()
+        {
+            List<Observer> observers = new();
+            Observer o1 = new("Fauda", "Tarablus Ave, North Yemen");
+            observers.Add(o1);
+            o1 = new("Satoo", "123 Ben Ali shara, Aasimat at-Tunis");
+            observers.Add(o1);
+            o1 = new("Fee Masr", "23 Auqaat, as-Saaba");
+            observers.Add(o1);
+            return observers;
+    }
+        
     }
 }
