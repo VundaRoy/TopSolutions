6b98c5be340ee7ce84118557d669f51f2ec11dbe
Roy Chang <roychang@live.com.au>
2025-08-24 20:13:47 +1000

    loop comparisons

--- TopSolutions.ConsoleApp/Algorithms/Looping/LoopingMain.cs
+++ TopSolutions.ConsoleApp/Algorithms/Looping/LoopingMain.cs
@@ -13,36 +13,49 @@ namespace TopSolutions.ConsoleApp.Algorithms.Looping
         public static void Main(string[] args)
         {
             LoopingMain lm = new LoopingMain();
-            lm.InitArray(100000);
+            lm.InitArray(100);
             //foreach
             Console.WriteLine("foreach");
             StopStart(lm.ForEachLoop);
             //for
             Console.WriteLine("for");
             StopStart(lm.ForLoop);
+            //while
+            Console.WriteLine("while");
+            StopStart(lm.WhileLoop);
         }
         private static Stopwatch StopStart(Action process)
         {
+            var methodName = process.Method.Name;
             Stopwatch stopwatch = new();
 
             stopwatch.Start();
             process();
             stopwatch.Stop();
-            Console.WriteLine($"Elapsed Time for {nameof(process)} is {stopwatch.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Elapsed Time for {methodName} is {stopwatch.ElapsedMilliseconds} ms");
             return stopwatch;
         }
         private void ForEachLoop()
         {
             foreach(var num in ArrayOfInts)
             {
-               // Console.WriteLine(num);
+               Thread.Sleep(100);
             }
         }
         private void ForLoop()
         {
             for(int i = 0; i < ArrayOfInts.Length; i++)
             {
-                //Console.WriteLine(ArrayOfInts[i]);
+                Thread.Sleep(100);
+            }
+        }
+        private void WhileLoop()
+        {
+            int i = 0;
+            while (i < ArrayOfInts.Length)
+            { 
+                Thread.Sleep(100);
+                i++;
             }
         }
         private void InitArray(int size)
