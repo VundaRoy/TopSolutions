c04314bf667f7872b5c6ddccc9d2ed617d4336f3
Roy Chang <roychang@live.com.au>
2025-09-05 09:40:04 +1000

    private constructor example

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Constructors/PrivateConstructor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Constructors
+{
+    public class PrivateConstructor
+    {
+        private PrivateConstructor()
+        {
+
+        }
+        public PrivateConstructor(string endPoint)
+        {
+            Console.Write(endPoint);
+        }
+        public void SomeMethod()
+        {
+            Console.WriteLine("Some method in private constructor");
+        }
+        static void Main(string[] args)
+        {
+            PrivateConstructor pc = new PrivateConstructor();
+            pc.SomeMethod();
+        }
+    }
+    public class Outsider
+    {
+        static void Main(string[] args)
+        {
+            //PrivateConstructor pc = new PrivateConstructor();//illegal
+            PrivateConstructor pc = new PrivateConstructor("wholesome");
+            pc.SomeMethod();
+
+        }
+    }
+}
