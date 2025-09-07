da1d3342aaaaba829c240e45178e15ab2fd82b0b
Roy Chang <roychang@live.com.au>
2025-09-08 09:25:39 +1000

    Tree nodes init

--- TopSolutions.ConsoleApp/Algorithms/Trees/Basic/BasicTreeMain.cs
+++ TopSolutions.ConsoleApp/Algorithms/Trees/Basic/BasicTreeMain.cs
@@ -8,35 +8,38 @@ namespace TopSolutions.ConsoleApp.Algorithms.Trees.Basic
 {
     public class BasicTreeMain
     {
+        List<int> nodes = new List<int> { 10,22,34,56,89,100,134,237,258,310};
         public static void Main(string[] args)
         {
             BinaryTree tree = new BinaryTree();
-
-            // Insert values
-            tree.Insert(50);
-            tree.Insert(30);
-            tree.Insert(70);
-            tree.Insert(20);
-            tree.Insert(40);
-            tree.Insert(60);
-            tree.Insert(80);
+            BasicTreeMain btm = new();
+            btm.InsertNodes(tree);
 
             // Perform traversals
             Console.Write("In-order traversal: ");
-            tree.InOrderTraversal(); // Output: 20 30 40 50 60 70 80
+            tree.InOrderTraversal(); //10 22 34 56 89 100 134 237 258 310
             Console.WriteLine();
 
             Console.Write("Pre-order traversal: ");
-            tree.PreOrderTraversal(); // Output: 50 30 20 40 70 60 80
+            tree.PreOrderTraversal(); //10 22 34 56 89 100 134 237 258 310
             Console.WriteLine();
 
             Console.Write("Post-order traversal: ");
-            tree.PostOrderTraversal(); // Output: 20 40 30 60 80 70 50
+            tree.PostOrderTraversal(); //310 258 237 134 100 89 56 34 22 10
             Console.WriteLine();
 
             // Search for a value
             Console.WriteLine("Search 40: " + tree.Search(40)); // Output: True
             Console.WriteLine("Search 90: " + tree.Search(90)); // Output: False
+        }
+
+        private void InsertNodes(BinaryTree bt)             
+        {
+            foreach(var  node in nodes)
+            {
+                bt.Insert(node);
+            }
+
         }
     }
 }
