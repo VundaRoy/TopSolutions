ff5a2413e30ff7a7f8e27f2f2d3230e03528b180
Roy Chang <roychang@live.com.au>
2025-09-11 09:07:36 +1000

    Sturct as main program

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Structs/Inherit/StructMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Structs.Inherit
+{
+    public struct StructMain
+    {
+        public static void Main(string[] args)
+        {
+            ShoppingCart shoppingCart = new ShoppingCart();
+            Console.WriteLine($"This struct says {shoppingCart.GetInfoByFormId(5)}.");
+        }
+    }
+}
