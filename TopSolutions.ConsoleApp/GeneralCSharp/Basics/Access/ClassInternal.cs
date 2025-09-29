841638dc7e63e2f61ae6eb2870afad8a747815c1
Roy Chang <roychang@live.com.au>
2025-09-29 11:21:19 +1000

    use of internal public and private classes

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Basics/Access/ClassInternal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Basics.Access
+{
+    internal class ClassInternal
+    {
+        public void OpenSesame()
+        {
+            Console.WriteLine("This is a public method.");
+        }
+
+    }
+    class ClassPrivate //Internal by default
+    {
+        private void OpenSesame()
+        {
+            Console.WriteLine("This is a private method.");
+        }
+        public void Test()
+        {
+            OpenSesame();
+        }
+    }
+
+    public class ClassPublic 
+    {
+        internal void OpenSesame()
+        {
+            Console.WriteLine("This is an internal method.");
+        }
+
+        public static void Main()
+        {
+            ClassInternal ci = new ClassInternal();
+            ci.OpenSesame();
+            ClassPublic cp = new ClassPublic();
+            cp.OpenSesame();
+            ClassPrivate cpr = new ClassPrivate();
+            // cpr.OpenSesame(); // Not accessible
+            cpr.Test(); // call private method via public method
+        }
+    }
+}
