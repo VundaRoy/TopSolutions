fd9aa3d495e681d33b919d448af004807357b989
Roy Chang <roychang@live.com.au>
2025-10-22 10:32:19 +1100

    literals sample

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Basics/Literals/LiteralMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Basics.Literals
+{
+    public class LiteralMain
+    {
+        //litrals examples
+        public static void Main()
+        {
+            // Integer literal
+            int intLiteral = 100;
+            Console.WriteLine($"Integer Literal: {intLiteral}");
+            // Floating-point literal
+            double floatLiteral = 99.99;
+            Console.WriteLine($"Floating-point Literal: {floatLiteral}");
+            // Character literal
+            char charLiteral = 'A';
+            Console.WriteLine($"Character Literal: {charLiteral}");
+            // String literal
+            string stringLiteral = "Hello, World!";
+            Console.WriteLine($"String Literal: {stringLiteral}");
+            // Boolean literal
+            bool boolLiteral = true;
+            Console.WriteLine($"Boolean Literal: {boolLiteral}");
+            // Null literal
+            string nullLiteral = null;
+            Console.WriteLine($"Null Literal: {nullLiteral}");
+            //unsigned integer literal
+            uint uintLiteral = 300U;
+            Console.WriteLine($"Unsigned Integer Literal: {uintLiteral}");
+            //long integer literal
+            long longLiteral = 5000L;
+            Console.WriteLine($"Long Integer Literal: {longLiteral}");
+            //unsigned long integer literal
+            ulong ulongLiteral = 7000UL;
+            Console.WriteLine($"Unsigned Long Integer Literal: {ulongLiteral}");
+        }
+    }
+}
