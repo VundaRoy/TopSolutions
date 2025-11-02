72bf456b9a4d63f39d38ba07d1ff4ea410ac5d5f
Roy Chang <roychang@live.com.au>
2025-11-03 09:57:34 +1100

    Abstract class and multi level inheritance

--- TopSolutions.ConsoleApp/OOP/Inheritance/Abstract/AbstractMain.cs
+++ TopSolutions.ConsoleApp/OOP/Inheritance/Abstract/AbstractMain.cs
@@ -20,6 +20,14 @@ namespace TopSolutions.ConsoleApp.OOP.Inheritance.Abstract
             saasStaff.Promote("Senior Developer", 5000);
             //Terminate SaaS Company staff
             saasStaff.Terminate();
+            //Create instance of EToroTraders
+            EToroTraders etoroTrader = new EToroTraders();
+            //Calculate bonus for eToro Trader
+            etoroTrader.CalculateBonus();
+            //Promote eToro Trader
+            etoroTrader.Promote("Senior Trader", 7000);
+            //Terminate eToro Trader - this will call the sealed method from SaasCompany
+            etoroTrader.Terminate();
         }
     }
 }
---
+++ TopSolutions.ConsoleApp/OOP/Inheritance/Abstract/EToroTraders.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.OOP.Inheritance.Abstract
+{
+    public class EToroTraders : SaasCompany
+    {
+        //implement CalculateBonus method
+        public override decimal CalculateBonus()
+        {
+            Console.WriteLine("Calculating bonus for eToro Traders...");
+            //if end of year, give 20% bonus
+            Console.WriteLine("End of year bonus is 20% of salary.");
+            return 0; // Placeholder implementation
+        }
+        //implement a sealed method
+        public sealed override void Promote(string newPosition, decimal salaryIncrease)
+        {
+            Console.WriteLine("Promoting eToro Trader...");
+            base.Promote(newPosition, salaryIncrease);
+        }
+        //implement Terminate method will cause error as it is sealed in SaasCompany
+        //public override void Terminate()
+    }
+}
