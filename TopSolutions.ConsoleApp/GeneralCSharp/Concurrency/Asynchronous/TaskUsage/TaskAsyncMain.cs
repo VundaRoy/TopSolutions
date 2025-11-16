acebaca519c50439d7bdf89d54bcf1f77dd4105e
Roy Chang <roychang@live.com.au>
2025-11-17 09:15:38 +1100

    Task async run

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Concurrency/Asynchronous/TaskUsage/TaskAsyncMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Concurrency.Asynchronous.TaskUsage
+{
+    public class TaskAsyncMain
+    {
+        //Running a task asynchronously in Main method
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Task Async Main Method Started......");
+            Task task1 = Task.Run(() => { PrintCounter(); });
+            Console.WriteLine($"Main Thread : {Thread.CurrentThread.ManagedThreadId} Completed");
+            Console.ReadKey();
+        }
+        static void PrintCounter()
+        {
+            Console.WriteLine($"Child Thread : {Thread.CurrentThread.ManagedThreadId} Started");
+            for (int count = 1; count <= 5; count++)
+            {
+                Console.WriteLine($"count value: {count}");
+            }
+            Console.WriteLine($"Child Thread : {Thread.CurrentThread.ManagedThreadId} Completed");
+        }
+    }
+}
