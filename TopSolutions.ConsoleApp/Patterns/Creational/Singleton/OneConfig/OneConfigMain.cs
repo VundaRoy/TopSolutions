894f53db21a1c7f8e01706c9a0722050b224de95
Roy Chang <roychang@live.com.au>
2025-11-25 10:22:02 +1100

    Singleton instance access

---
+++ TopSolutions.ConsoleApp/Patterns/Creational/Singleton/OneConfig/OneConfigMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Creational.Singleton.OneConfig
+{
+    public class OneConfigMain
+    {
+        public static void Main(string[] args)
+        {
+            OneConfigToRule config1 = OneConfigToRule.Instance;
+            OneConfigToRule config2 = OneConfigToRule.Instance;
+            if (config1 == config2)
+            {
+                Console.WriteLine("Both config1 and config2 reference the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("config1 and config2 reference different instances.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/Patterns/Creational/Singleton/OneConfig/OneConfigToRule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Creational.Singleton.OneConfig
+{
+    public class OneConfigToRule
+    {
+        private static OneConfigToRule _instance;
+        //Singleton pattern for one config to rule them all
+        public static OneConfigToRule Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new OneConfigToRule();
+                }
+                return _instance;
+            }
+        }
+        
+    }
+}
