1ebd050e0f882251907da2b783b806a4319d538c
Roy Chang <roychang@live.com.au>
2025-12-01 09:14:39 +1100

    refactor ATC for brevity

--- TopSolutions.ConsoleApp/Patterns/Behavioural/Mediator/ATC/ClientATCMain.cs
+++ TopSolutions.ConsoleApp/Patterns/Behavioural/Mediator/ATC/ClientATCMain.cs
@@ -8,19 +8,24 @@
             // Register two runways
             controlTower.RegisterRunway(new Runway("R1"));
             controlTower.RegisterRunway(new Runway("R2"));
-            var airplane1 = new Airplane("FL123", controlTower);
-            var airplane2 = new Airplane("FL456", controlTower);
-            var airplane3 = new Airplane("FJ522", controlTower);
-            var cathay = new Airplane("CA100", controlTower);
-            var malaysian1 = new Airplane("MH105", controlTower);
-            airplane1.RequestLanding();
-            airplane2.RequestLanding();
-            airplane3.RequestLanding();
-            cathay.RequestLanding();
-            malaysian1.RequestLanding();
-            // Only one plane should land as we only have one runway. 
-            // The other should wait for an available runway.
+            
+            var listOfPlanes = new ClientATCMain().CreateAirplanes(controlTower);
+            foreach (var plane in listOfPlanes)
+            {
+                plane.RequestLanding();
+            }
             Console.ReadKey();
+        }
+        public List<Airplane> CreateAirplanes(IControlTower controlTower)
+        {
+            return new List<Airplane>
+            {
+                new Airplane("FL123", controlTower),
+                new Airplane("FL456", controlTower),
+                new Airplane("FJ522", controlTower),
+                new Airplane("CA100", controlTower),
+                new Airplane("MH105", controlTower)
+            };
         }
     }
 }
