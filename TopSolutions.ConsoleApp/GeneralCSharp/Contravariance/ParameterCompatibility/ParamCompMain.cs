ec8115e2e8cbb24ed7a3716e41985504c85094f4
Roy Chang <roychang@live.com.au>
2025-12-08 10:30:53 +1100

    contravariance delegate parameter

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Contravariance/ParameterCompatibility/ParamCompMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Contravariance.ParameterCompatibility
+{
+    delegate void StringDelegate(string str);
+    delegate void IntegerDelegate(int num);
+    delegate void DecimalDelegate(decimal dec);
+    internal class ParamCompMain
+    {
+        public static void Main()
+        {
+            StringDelegate del = new (ActionObject);
+            del("Hello, World!");
+            //IntegerDelegate delInt = new IntegerDelegate(ActionString); // This line would cause a compile-time error
+            
+        }
+        static void ActionObject(object obj)
+        {
+            Console.WriteLine("Object: " + obj);
+        }
+    }
+}
