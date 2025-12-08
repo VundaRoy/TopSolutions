fa75d81a69d21e75bef320ae1a6859edf75e0583
Roy Chang <roychang@live.com.au>
2025-12-09 08:47:48 +1100

    Different param type for delegate object call

--- TopSolutions.ConsoleApp/GeneralCSharp/Contravariance/ParameterCompatibility/ParamCompMain.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Contravariance/ParameterCompatibility/ParamCompMain.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace TopSolutions.ConsoleApp.GeneralCSharp.Contravariance.ParameterCompatibility
 {
     delegate void StringDelegate(string str);
+    delegate void AnotherStringDelegate(string str);
     delegate void IntegerDelegate(int num);
     delegate void DecimalDelegate(decimal dec);
     internal class ParamCompMain
@@ -16,11 +17,18 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Contravariance.ParameterCompatib
             StringDelegate del = new (ActionObject);
             del("Hello, World!");
             //IntegerDelegate delInt = new IntegerDelegate(ActionString); // This line would cause a compile-time error
+            //call another string delgate
+            AnotherStringDelegate del2 = new (AnotherStringDelegate);
+            del2("Yes another one type");
             
         }
         static void ActionObject(object obj)
         {
             Console.WriteLine("Object: " + obj);
+        }
+        static void AnotherStringDelegate(object obj)
+        {
+            Console.WriteLine("Another string type: " + obj);
         }
     }
 }
