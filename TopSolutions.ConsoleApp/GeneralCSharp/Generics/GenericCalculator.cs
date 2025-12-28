ebdcf32adb3c2efd75f076bb85bd98db9045de97
Roy Chang <roychang@live.com.au>
2025-12-28 21:37:57 +1100

    null check

--- TopSolutions.ConsoleApp/GeneralCSharp/Generics/GenericCalculator.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Generics/GenericCalculator.cs
@@ -10,10 +10,19 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Generics
     {
         public static bool AreEqual<T>(T value1, T value2)
         {
+            //ensure value1 is not null to avoid NullReferenceException
+            if(value1 == null)
+            {
+                return value2 == null;
+            }
             return value1.Equals(value2);
         }
         public static bool AreTwoTypesEqual<T1, T2>(T1 value1, T2 value2)
         {
+            if(value1 == null || value2 == null)
+            {
+                return value1 == null && value2 == null;
+            }
             return value1.Equals(value2);
         }
     }
