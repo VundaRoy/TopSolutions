2df4abca5b9689af546c90222575d0bd0fb93da4
Roy Chang <roychang@live.com.au>
2026-01-06 10:23:54 +1100

    tasks and locking

--- TopSolutions.ConsoleApp/GeneralCSharp/Concurrency/Locking/LockingMain.cs
+++ TopSolutions.ConsoleApp/GeneralCSharp/Concurrency/Locking/LockingMain.cs
@@ -8,14 +8,17 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Concurrency.Locking
 {
     public class LockingMain
     {
+        static string[] fijianCount = new string[] { "dua (F)", "rua", "tolu", "va", "lima" };
+        static string[] malayCount = new string[] { "satu", "dua", "tiga", "empat", "lima" };
+        static string[] maoriCount = new string[] { "tahi", "rua", "toru", "wha", "rima" };
         //Locking example in C# 
         private static readonly object _lockObject = new ();
         public static void Main(string[] args)
         {
             //locking example with multiple threads
-            Thread t1 = new Thread(AccessSharedResource);
-            Thread t2 = new Thread(AccessSharedResource);
-            Thread t3 = new Thread(AccessSharedResource);
+            Thread t1 = new(AccessSharedResource);
+            Thread t2 = new (AccessSharedResource);
+            Thread t3 = new (AccessSharedResource);
             t1.Start();
             t2.Start();
             t3.Start();
@@ -24,13 +27,13 @@ namespace TopSolutions.ConsoleApp.GeneralCSharp.Concurrency.Locking
         //Method to access a shared resource with locking
         public static void AccessSharedResource()
         {
-            Console.WriteLine($"Task is waiting to access the shared resource - tua...");
+            Console.WriteLine($"Task is waiting to access the shared resource - {fijianCount[0]}...");
             lock (_lockObject)
             {
-                Console.WriteLine($"Task is waiting to access the shared resource - lua...");
+                Console.WriteLine($"Task is waiting to access the shared resource - {malayCount[1]}...");
                 // Simulate work with the shared resource
                 Task.Delay(1000).Wait();
-                Console.WriteLine($"Task is waiting to access the shared resource - tolu...");
+                Console.WriteLine($"Task is waiting to access the shared resource -  {maoriCount[2]} .....");
             }
         }
     }
