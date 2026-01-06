ace79abe4af44cc2e4b9f0438693e23722ba8609
Roy Chang <roychang@live.com.au>
2026-01-07 09:46:20 +1100

    string conversion methods

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Basics/Strings/StringConversion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Basics.Strings
+{
+    internal class StringConversion
+    {
+        public static void Main(string[] args)
+        {
+            //Convert object to string using ToString()
+            object obj = 12345;
+            string str1 = obj.ToString();
+            Console.WriteLine("Convert using ToString()" +str1);
+            //Convert object to string using Convert.ToString()
+            string str2 = Convert.ToString(obj);
+            //Convert string to int using int.Parse()
+            //convert null value to string using Convert.ToString()
+            string nullString = null;
+            string str3 = Convert.ToString(nullString);
+            Console.WriteLine("Convert null using Convert.ToString(): " + (str3 == null ? "null" : str3));
+            //convert null value to string using ToString() - this will throw an exception
+            //string str4 = nullString.ToString(); // Uncommenting this line will throw a NullReferenceException
+        }
+    }
+}
