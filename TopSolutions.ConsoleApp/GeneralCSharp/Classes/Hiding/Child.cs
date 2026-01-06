3d0a973b05c09de84852af48c9cbe4d3704eb6fa
Roy Chang <roychang@live.com.au>
2026-01-07 09:58:28 +1100

    method hiding with inheritance

---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Classes/Hiding/Child.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Classes.Hiding
+{
+    internal class Child : Parent
+    {
+        //Method hiding using 'new' keyword
+        public new void Display()
+        {
+            base.Display(); //calling parent class method
+            Console.WriteLine("Display method from Child class");
+        }
+        //method hiding without 'new' keyword  
+        public void Show()
+        {
+            Console.WriteLine("Show method from Child class");
+        }
+        //normal inheritance of parent class method
+        //public override void Show() // overriding the Show method says it is already defined in child class
+        //{
+        //    Console.WriteLine("Show method from Child class");
+        //}
+
+    }
+}
---
+++ TopSolutions.ConsoleApp/GeneralCSharp/Classes/Hiding/MethodHidingMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.GeneralCSharp.Classes.Hiding
+{
+    internal class MethodHidingMain
+    {
+        public static void Main(string[] args)
+        {
+            Parent parentRef;
+            Child childObj = new Child();
+            // Calling hidden method using child class object
+            childObj.Display(); // Output: Display method from Child class
+            // Calling hidden method using parent class reference
+            parentRef = childObj;
+            parentRef.Display(); // Output: Display method from Parent class
+            // Calling overridden method using child class object
+            childObj.Show(); // Output: Show method from Child class
+            // Calling hidden method using parent class reference
+            parentRef.Show(); // Output: Show method from Parent class
+        }
+    }
+}
