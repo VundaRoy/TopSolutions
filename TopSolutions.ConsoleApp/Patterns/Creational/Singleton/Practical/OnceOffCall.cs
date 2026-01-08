899423fb1a6d3a77295c128b79a8733af5397d1b
Roy Chang <roychang@live.com.au>
2026-01-08 11:27:17 +1100

    Singleton calls and exception

---
+++ TopSolutions.ConsoleApp/Patterns/Creational/Singleton/Practical/OnceOffCall.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Creational.Singleton.Practical
+{
+    internal class OnceOffCall
+    {
+        //Singleton that allows only one method call
+        private static OnceOffCall _instance;
+        private bool _hasBeenCalled = false;
+        private OnceOffCall() { }
+        public static OnceOffCall Instance
+        {
+            get
+            {
+                _instance ??= new OnceOffCall();
+                return _instance;
+            }
+        }
+        //properties of the singleton
+        public void ExecuteOnce()
+        {
+            if (_hasBeenCalled)
+            {
+                throw new InvalidOperationException("This method can only be called once.");
+            }
+            Console.WriteLine("Executing the once-off call.");
+            _hasBeenCalled = true;
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/Patterns/Creational/Singleton/Practical/PracticalSingletonMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Creational.Singleton.Practical
+{
+    internal class PracticalSingletonMain
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Practical Singleton Pattern Demo");
+            var onceOff1 = OnceOffCall.Instance;
+            var onceOff2 = OnceOffCall.Instance;
+            Console.WriteLine($"Are both OnceOffCall instances the same? {ReferenceEquals(onceOff1, onceOff2)}");
+            // Additional practical singleton usage can be demonstrated here
+            //call ExecuteOnce again
+            onceOff1.ExecuteOnce(); // This will work
+            onceOff2.ExecuteOnce(); // This will throw an exception
+
+            Console.WriteLine("End of Practical Singleton Pattern Demo");
+        }
+    }
+}
