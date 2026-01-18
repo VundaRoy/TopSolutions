b21961b52ac6f25c5dd05699dac38fd2feb8d782
Roy Chang <roychang@live.com.au>
2026-01-18 20:31:50 +1100

    Strategy pattern with payment

---
+++ TopSolutions.ConsoleApp/Patterns/Behavioural/Strategy/Payment/Checkout.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Behavioural.Strategy.Payment
+{
+    public class Checkout
+    {
+        private IPaymentStrategy _paymentStrategy; // Composition
+        public Checkout(IPaymentStrategy paymentStrategy)
+        {
+            _paymentStrategy = paymentStrategy;
+        }
+        public void setStrategy(IPaymentStrategy paymentStrategy) // Setter for changing strategy at runtime
+        {
+            _paymentStrategy = paymentStrategy;
+        }
+        public void ProcessOrder(decimal amount)
+        {
+            _paymentStrategy.Pay(amount);
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/Patterns/Behavioural/Strategy/Payment/PaymentMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Behavioural.Strategy.Payment
+{
+    public class PaymentMain
+    {
+        public static void Main()
+        {
+            Checkout checkout = new Checkout(new CreditCardParyment()); // Default strategy
+            checkout.ProcessOrder(100);
+            checkout.setStrategy(new PayPalPayment()); // Change strategy at runtime
+            checkout.ProcessOrder(200);
+        }
+
+    }
+}
