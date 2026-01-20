f5076fff41cafab0e227dec75501137cb90880c0
Roy Chang <roychang@live.com.au>
2026-01-21 09:58:45 +1100

    Saga sample

---
+++ TopSolutions.ConsoleApp/Patterns/Saga/InventoryMaster/Components/IService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Saga.InventoryMaster.Components
+{
+    public interface IService
+    {
+        bool Do(OrderSagaState state);
+        void Undo(OrderSagaState state);
+    }
+}
---
+++ TopSolutions.ConsoleApp/Patterns/Saga/InventoryMaster/Components/OrderSagaState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Saga.InventoryMaster.Components
+{
+    public class OrderSagaState
+    {
+        public int OrderId { get; set; }
+        public bool PaymentCompleted { get; set; }
+        public bool InventoryReserved { get; set; }
+    }
+}
