7d8fcccbedea0450d72f1c05d346e1c8c1cc944f
Roy Chang <roychang@live.com.au>
2026-01-28 21:16:09 +1100

    Refactor builder computer

--- TopSolutions.ConsoleApp/Patterns/Creational/Builder/Computer/BuilderComputerMain.cs
+++ TopSolutions.ConsoleApp/Patterns/Creational/Builder/Computer/BuilderComputerMain.cs
@@ -11,16 +11,42 @@ namespace TopSolutions.ConsoleApp.Patterns.Creational.Builder.Computer
         public static void Main()
         {
             ComputerShop shop = new();
-            ComputerBuilder builder = new GamingComputerBuilder();
-            shop.ConstructComputer(builder);
-            Computer computer = builder.GetComputer();
-            computer.DisplaySpecifications();
+            GamerComputer(shop);
+            DeveloperComputer(shop);
+            HighSchoolComputer(shop);
 
+        }
+
+        private static void HighSchoolComputer(ComputerShop shop)
+        {
+            //Highschool computer
+            Console.WriteLine("Highschool Computer Specifications:");
+            ComputerBuilder hsPC = new HighSchoolerComputerBuilder();
+            shop.ConstructComputer(hsPC);
+            Computer comp3 = hsPC.GetComputer();
+            comp3.DisplaySpecifications();
+            Console.ReadKey();
+        }
+
+        private static void DeveloperComputer(ComputerShop shop)
+        {
+            //Developer computer
+            Console.WriteLine("Developer Computer Specifications:");
             ComputerBuilder devPC = new DeveloperComputerBuilder();
             shop.ConstructComputer(devPC);
             Computer comp2 = devPC.GetComputer();
             comp2.DisplaySpecifications();
             Console.ReadKey();
+        }
+
+        private static void GamerComputer(ComputerShop shop)
+        {
+            //Gaming computer
+            Console.WriteLine("Gaming Computer Specifications:");
+            ComputerBuilder builder = new GamingComputerBuilder();
+            shop.ConstructComputer(builder);
+            Computer computer = builder.GetComputer();
+            computer.DisplaySpecifications();
         }
     }
 }
---
+++ TopSolutions.ConsoleApp/Patterns/Creational/Builder/Computer/HighSchoolerComputerBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Creational.Builder.Computer
+{
+    public class HighSchoolerComputerBuilder : ComputerBuilder
+    {
+        public override void SetCPU()
+        {
+            Computer.CPU = "Intel i3";
+        }
+        public override void SetHardDrive()
+        {
+            Computer.HardDrive = "HDD 500 GB";
+        }
+        public override void SetManufacturer()
+        {
+            Computer.Manufacturer = "Dell Inspiron";
+        }
+        public override void SetMicrosoftApps()
+        {
+            Computer.MicrosoftApps = "MS Office Student Edition";
+        }
+        public override void SetRAM()
+        {
+            Computer.RAM = "8 GB RAM";
+        }
+        public override void SetSoundCard()
+        {
+            Computer.SoundCard = "Integrated sound card";
+        }
+    }
+}
