e15b2afd1448d6bdc2b5e4bb24ee8a0b2499098b
Roy Chang <roychang@live.com.au>
2026-01-29 09:22:10 +1100

    Add new house type to builder

---
+++ TopSolutions.ConsoleApp/Patterns/Creational/Builder/House/ChateauMansionBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Creational.Builder.House
+{
+    public class ChateauMansionBuilder : IHouseBuilder
+    {
+        private House house;
+        public ChateauMansionBuilder()
+        {
+            this.house = new House();
+        }
+        public void BuildBasement()
+        {
+            house.SetBasement("Stone Basement");
+        }
+        public void BuildStructure()
+        {
+            house.SetStructure("Stone and Marble Structure");
+        }
+        public void BuildRoof()
+        {
+            house.SetRoof("Slate Roof");
+        }
+        public void BuildInterior()
+        {
+            house.SetInterior("Luxury Interior");
+        }
+        public House GetHouse()
+        {
+            return house;
+        }
+    
+    }
+}
--- TopSolutions.ConsoleApp/Patterns/Creational/Builder/House/HouseMain.cs
+++ TopSolutions.ConsoleApp/Patterns/Creational/Builder/House/HouseMain.cs
@@ -10,18 +10,42 @@ namespace TopSolutions.ConsoleApp.Patterns.Creational.Builder.House
     {
         public static void Main()
         {
-            IHouseBuilder iglooBuilder = new IglooHouseBuilder();
-            CivilEngineer engineer = new CivilEngineer(iglooBuilder);
-            engineer.ConstructHouse();
-            House house = engineer.GetHouse();
-            Console.WriteLine("House built:\n" + house);
+            //build Igloo
+            CivilEngineer engineer;
+            House house;
+            IglooBuilder(out engineer, out house);
             //build Yurt
+            YurtBuilder(out engineer, out house);
+            //build Chateau Mansion
+            ChateauMansionBuilder(out engineer, out house);
+
+        }
+
+        private static void ChateauMansionBuilder(out CivilEngineer engineer, out House house)
+        {
+            IHouseBuilder chateauBuilder = new ChateauMansionBuilder();
+            engineer = new CivilEngineer(chateauBuilder);
+            engineer.ConstructHouse();
+            house = engineer.GetHouse();
+            Console.WriteLine("House built:\n" + house);
+        }
+
+        private static void YurtBuilder(out CivilEngineer engineer, out House house)
+        {
             IHouseBuilder yurtBuilder = new YurtHouseBuilder();
             engineer = new CivilEngineer(yurtBuilder);
             engineer.ConstructHouse();
             house = engineer.GetHouse();
             Console.WriteLine("House built:\n" + house);
+        }
 
+        private static void IglooBuilder(out CivilEngineer engineer, out House house)
+        {
+            IHouseBuilder iglooBuilder = new IglooHouseBuilder();
+            engineer = new CivilEngineer(iglooBuilder);
+            engineer.ConstructHouse();
+            house = engineer.GetHouse();
+            Console.WriteLine("House built:\n" + house);
         }
     }
 }
