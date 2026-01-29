9493ce1bbe7dc275593b47484b875136c3078ef6
Roy Chang <roychang@live.com.au>
2026-01-30 09:59:29 +1100

    Refactor computer builder to single helper call

--- TopSolutions.ConsoleApp/Patterns/Creational/Builder/Computer/BuilderComputerMain.cs
+++ TopSolutions.ConsoleApp/Patterns/Creational/Builder/Computer/BuilderComputerMain.cs
@@ -11,42 +11,26 @@ namespace TopSolutions.ConsoleApp.Patterns.Creational.Builder.Computer
         public static void Main()
         {
             ComputerShop shop = new();
-            GamerComputer(shop);
-            DeveloperComputer(shop);
-            HighSchoolComputer(shop);
-
+            // One line per configuration
+            BuildAndPrint(shop, "Gaming Computer Specifications:", () => new GamingComputerBuilder());
+            BuildAndPrint(shop, "Developer Computer Specifications:", () => new DeveloperComputerBuilder());
+            BuildAndPrint(shop, "Highschool Computer Specifications:", () => new HighSchoolerComputerBuilder());
         }
 
-        private static void HighSchoolComputer(ComputerShop shop)
+        private static Computer BuildAndPrint(ComputerShop shop, string heading, Func<ComputerBuilder> builderFactory)
         {
-            //Highschool computer
-            Console.WriteLine("Highschool Computer Specifications:");
-            ComputerBuilder hsPC = new HighSchoolerComputerBuilder();
-            shop.ConstructComputer(hsPC);
-            Computer comp3 = hsPC.GetComputer();
-            comp3.DisplaySpecifications();
-            Console.ReadKey();
-        }
+            Console.WriteLine(heading);
 
-        private static void DeveloperComputer(ComputerShop shop)
-        {
-            //Developer computer
-            Console.WriteLine("Developer Computer Specifications:");
-            ComputerBuilder devPC = new DeveloperComputerBuilder();
-            shop.ConstructComputer(devPC);
-            Computer comp2 = devPC.GetComputer();
-            comp2.DisplaySpecifications();
-            Console.ReadKey();
-        }
-
-        private static void GamerComputer(ComputerShop shop)
-        {
-            //Gaming computer
-            Console.WriteLine("Gaming Computer Specifications:");
-            ComputerBuilder builder = new GamingComputerBuilder();
+            var builder = builderFactory();
             shop.ConstructComputer(builder);
-            Computer computer = builder.GetComputer();
+
+            var computer = builder.GetComputer();
             computer.DisplaySpecifications();
+
+            // If pausing is desired after each build, uncomment:
+            // Console.ReadKey();
+
+            return computer;
         }
     }
 }
