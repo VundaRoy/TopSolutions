c8c2f09d98f590d929e998cff0d4131d077c7614
Roy Chang <roychang@live.com.au>
2026-02-06 09:37:04 +1100

    Instance factory class for decorator pattern demo

---
+++ TopSolutions.ConsoleApp/Patterns/Structural/Decorator/Car/DecoratorCarInstance.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Structural.Decorator.Car
+{
+    public class DecoratorCarInstance
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new CarFactory();
+
+            // Plain BMW without engine
+            ICar bmwCar1 = new BMWCar();
+            bmwCar1.ManufactureCar();
+            Console.WriteLine(bmwCar1 + "\n");
+
+            // BMW with Diesel Engine
+            var carWithDieselEngine = factory.CreateDecoratedCar<DieselCarDecorator>();
+            carWithDieselEngine.ManufactureCar();
+            Console.WriteLine();
+
+            // BMW with Petrol Engine
+            var carWithPetrolEngine = factory.CreateDecoratedCar<PetrolCarDecorator>();
+            carWithPetrolEngine.ManufactureCar();
+
+            // BMW Hybrid
+            var carHybrid = factory.CreateDecoratedCar<HybridCarDecorator>();
+            carHybrid.ManufactureCar();
+            carHybrid.ManageBattery();
+            carHybrid.MonitorFuelEfficiency();
+
+            // BMW Electric
+            var bmwElec = factory.CreateDecoratedCar<ElectricCarDecorator>();
+            bmwElec.ManufactureCar();
+            bmwElec.MonitorBatteryHealth();
+
+            Console.ReadKey();
+        }
+    }
+    class CarFactory
+    {
+        public T CreateDecoratedCar<T>() where T : class
+        {
+            ICar bmwCar = new BMWCar();
+            return (T)Activator.CreateInstance(typeof(T), bmwCar);
+        }
+    }
+}
