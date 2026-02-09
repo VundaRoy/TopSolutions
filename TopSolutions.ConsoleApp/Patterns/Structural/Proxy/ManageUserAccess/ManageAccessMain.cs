f452f1d51b0c7818f0c7835a35357155a5f03603
Roy Chang <roychang@live.com.au>
2026-02-10 10:16:24 +1100

    proxy with granting access

---
+++ TopSolutions.ConsoleApp/Patterns/Structural/Proxy/ManageUserAccess/ManageAccessMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Patterns.Structural.Proxy.ManageUserAccess
+{
+    public class ManageAccessMain
+    {
+        public static void Main(string[] args)
+        {
+            // Create the proxy
+            ManageAccessProxy proxy = new ();
+            //Grant access to a user
+            proxy.GrantAccess("Alice");
+            //Grant access to another user
+            proxy.GrantAccess("Bob");
+            //Display the access list
+            proxy.DisplayAccessList();
+            //Deny access to a user
+            proxy.DenyAccess("Alice");
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/Patterns/Structural/Proxy/ManageUserAccess/ManageAccessProxy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TopSolutions.ConsoleApp.Patterns.Structural.Proxy.ManageUserAccess
+{
+    public class ManageAccessProxy : IManageAccess
+    {
+      
+        private List<string> accessList;
+        public ManageAccessProxy()
+        {
+           
+            accessList = new List<string>();
+        }
+        public void GrantAccess(string user)
+        {
+            if (!accessList.Contains(user))
+            {
+                accessList.Add(user);
+                Console.WriteLine($"Granted access to {user}");
+            }
+            else
+            {
+                Console.WriteLine($"{user} already has access");
+            }
+        }
+        public void DenyAccess(string user)
+        {
+            if (accessList.Contains(user))
+            {
+                accessList.Remove(user);
+                Console.WriteLine($"Denied access to {user}");
+            }
+            else
+            {
+                Console.WriteLine($"{user} does not have access");
+            }
+        }
+        public void DisplayAccessList()
+        {
+            Console.WriteLine("Current Access List:");
+            foreach (var user in accessList)
+            {
+                Console.WriteLine(user);
+            }
+        }
+    }
+}
