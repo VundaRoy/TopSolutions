fb5cf72b06a23adc79527e5f4bff0cb14325d374
Roy Chang <roychang@live.com.au>
2026-02-25 11:48:22 +1100

    Refactor cook factory main

--- TopSolutions.ConsoleApp/Patterns/Creational/Factory/Simple/MainCook.cs
+++ TopSolutions.ConsoleApp/Patterns/Creational/Factory/Simple/MainCook.cs
@@ -3,26 +3,27 @@
 namespace TopSolutions.ConsoleApp.Patterns.Creational.Factory.Simple
 {
     public class MainCook
-    {        
-        const string AsianIngredients =  "Bok choi, celery, carrot, onions";
+    {
+        const string AsianIngredients = "Bok choi, celery, carrot, onions";
         const string LatinIngredients = "Habichuela, Tamale, Yucca";
-        
+
         static void Main(string[] args)
         {
-            string type = "Asian";
-            var cook = CookFactory.GetCookType(type);
+           //Cook based on type
+            var mainCook = new MainCook();
+            mainCook.CookBasedOnType("Asian");
+            mainCook.CookBasedOnType("Latin");
 
+        }
+        public void CookBasedOnType(string type)
+        {
+            var cook = CookFactory.GetCookType(type);
             if (cook != null)
             {
                 cook.CookFood(AsianIngredients);
                 cook.ServerFood();
-                type = "Latin";
-                cook = CookFactory.GetCookType(type);
-                cook.CookFood(LatinIngredients);
-                cook.ServerFood();
             }
 
         }
-
     }
 }
