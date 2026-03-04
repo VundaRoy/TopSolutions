7e60aeaf16361226e7106b74dbf5cb0ee73a415f
Roy Chang <roychang@live.com.au>
2026-03-05 09:32:53 +1100

    LSP working and not working

--- TopSolutions.ConsoleApp/SOLID/LSP/Basic/Violate/LiskovViolateMain.cs
+++ TopSolutions.ConsoleApp/SOLID/LSP/Basic/Violate/LiskovViolateMain.cs
@@ -14,6 +14,12 @@ namespace TopSolutions.ConsoleApp.SOLID.LSP.Basic.Violate
             rect.ChangeDimensions(rect, 4, 5);  // This works fine
             var square = new Square { Base = 2 };
             rect.ChangeDimensions(square, 4, 5);  // This behaves unexpectedly!
+            //square
+            var square2 = new Square { Base = 2 };
+            square2.ChangeDimensions(square2, 4, 5);  // This behaves unexpectedly!
+            //square that works fine
+            var square3 = new Square { Base = 2 };
+            square3.ChangeDimensions(square3, 4, 4);  // This works fine
             var triangle = new Triangle { Base = 2 };
             rect.ChangeDimensions(triangle, 4, 5);
             Console.ReadKey();
