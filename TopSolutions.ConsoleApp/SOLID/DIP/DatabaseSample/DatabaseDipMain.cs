22a51f9f3ea8737e935c8c11835bbcef3696202c
Roy Chang <roychang@live.com.au>
2026-03-12 09:46:50 +1100

    refactor DIP main

--- TopSolutions.ConsoleApp/SOLID/DIP/DatabaseSample/DatabaseDipMain.cs
+++ TopSolutions.ConsoleApp/SOLID/DIP/DatabaseSample/DatabaseDipMain.cs
@@ -10,15 +10,38 @@ namespace TopSolutions.ConsoleApp.SOLID.DIP.DatabaseSample
     {
         public static void Main(string[] args)
         {
-            var logger = new Logger(new DatabaseService(), new LoggingService());  //initialize the logger with the concrete implementations of the services          
+            DatabaseDipMain dipMain = new();
+
+            //factory method to create logger instance, this is the only place where we create the logger and we can easily change the implementation
+            //of the logger without affecting the rest of the code
+
+            var logger = dipMain.CreateLogger();
             logger.Log("Hello, no errors");
             logger.WriteToLog("Logged at " + DateTime.Now);
             logger.ArchiveLogs();
             logger.RollbackTransaction("myId12");
-            var comms = new Comms(new  DatabaseService());
-            comms.SendComms("Welcome to the channel");
-            logger.RollbackTransaction("1123");
-            comms.UnSendMessage("Theo was right");
+            dipMain.SendComms("This is a message to send");
+            dipMain.UnSendComms("This is a message to unsend");
+        }
+        //Create new logger instance
+        public Logger CreateLogger()
+        {
+            return new Logger(new DatabaseService(), new LoggingService());
+        }
+        public void SendComms(string message)
+        {
+            var comms = new Comms(new DatabaseService());
+            comms.SendComms(message);
+        }
+        public void UnSendComms(string message)
+        {
+            var comms = new Comms(new DatabaseService());
+            comms.UnSendMessage(message);
+        }
+        public void WriteToLog(string message)
+        {
+            var logger = new Logger(new DatabaseService(), new LoggingService());
+            logger.WriteToLog(message);
         }
     }
 }
