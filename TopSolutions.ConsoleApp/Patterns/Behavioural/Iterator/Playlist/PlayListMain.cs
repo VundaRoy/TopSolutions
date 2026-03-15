9556f669f8a8e695e96b3490ff2a68f8860ff842
Roy Chang <roychang@live.com.au>
2026-03-16 09:13:13 +1100

    iterator work out

--- TopSolutions.ConsoleApp/Patterns/Behavioural/Iterator/Playlist/PlayListMain.cs
+++ TopSolutions.ConsoleApp/Patterns/Behavioural/Iterator/Playlist/PlayListMain.cs
@@ -16,12 +16,35 @@ namespace TopSolutions.ConsoleApp.Patterns.Behavioural.Iterator.Playlist
             myPlaylist.AddSong(new Song("Song Song Blue", "Neil Diamond"));
             myPlaylist.AddSong(new Song("Rising Sun", "Animals"));
             myPlaylist.AddSong(new Song("Saajan Sasural", "Jagjit Singh"));
+            myPlaylist.AddSong(new Song("Mere Dholna", "Shreya Ghoshal"));
             IIterator<Song> iterator = myPlaylist.CreateIterator();
             while (iterator.HasNext())
             {
                 Console.WriteLine(iterator.Next());
             }
+            //Get list of English songs
+            var englishSongs = new PlayListMain().GetListOfEnglishSongs(myPlaylist);
+            Console.WriteLine("List of English songs:");
+            IIterator<Song> englishSongsIterator = englishSongs.CreateIterator();
+            while (englishSongsIterator.HasNext())
+            {
+                Console.WriteLine(englishSongsIterator.Next());
+            }
             Console.ReadLine();
+        }
+        public Playlist GetListOfEnglishSongs(Playlist playlist)
+        {
+            var englishSongs = new Playlist();
+            IIterator<Song> iterator = playlist.CreateIterator();
+            while (iterator.HasNext())
+            {
+                var song = iterator.Next();
+                if (song.Artist != "SP Bala" && song.Artist != "Jagjit Singh" && song.Artist != "Shreya Ghoshal")
+                {
+                    englishSongs.AddSong(song);
+                }
+            }
+            return englishSongs;
         }
     }
 }
