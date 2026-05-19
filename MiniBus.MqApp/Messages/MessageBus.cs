d53819f593ab4bbf5534be49af3600a7b2b4bfab
Roy Chang <roychang@live.com.au>
2026-05-20 09:41:28 +1000

    Add queueing logic

--- MiniBus.MqApp/Messages/MessageBus.cs
+++ MiniBus.MqApp/Messages/MessageBus.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using MiniBus.MqApp.Core;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,19 +13,47 @@ namespace MiniBus.MqApp.Messages
     {
 
         private readonly IServiceProvider _provider;
+        private readonly BlockingCollection<IMessage> _queue;
 
         public MessageBus(IServiceProvider provider)
         {
             _provider = provider;
+            _queue = new BlockingCollection<IMessage>();
         }
 
         public async Task Publish<T>(T message) where T : IMessage
         {
-            var consumers = _provider.GetServices<IConsumer<T>>();
+            //var consumers = _provider.GetServices<IConsumer<T>>();
+
+            //foreach (var consumer in consumers)
+            //{
+            //    await consumer.Consume(message);
+            //}
+            _queue.Add(message); //enqueue the message for processing instead of processing it immediately
+        }
+
+        public void Start()
+        {
+            Task.Run(() =>
+            {
+                foreach (var message in _queue.GetConsumingEnumerable())
+                {
+                    Dispatch(message);
+                }
+            });
+        }
+
+        private void Dispatch(IMessage message)
+        {
+            var messageType = message.GetType();
+            var consumerType = typeof(IConsumer<>).MakeGenericType(messageType);
+
+            var consumers = _provider.GetServices(consumerType);
 
             foreach (var consumer in consumers)
             {
-                await consumer.Consume(message);
+                var method = consumerType.GetMethod("Consume");
+                method.Invoke(consumer, new object[] { message });
             }
         }
 
--- MiniBus.MqApp/Program.cs
+++ MiniBus.MqApp/Program.cs
@@ -13,10 +13,18 @@ var provider = services.BuildServiceProvider();
 
 var bus = provider.GetRequiredService<MessageBus>();
 
-await bus.Publish(new OrderSubmitted
-{
-    OrderId = Guid.NewGuid()
-});
+bus.Start();  // start background queue processing
 
-Console.WriteLine("Done. Press any key...");
-Console.ReadKey();
+bus.Publish(new OrderSubmitted { OrderId = Guid.NewGuid() });
+
+Console.ReadLine();
+
+//Commented out this section to demonstrate that messages are now enqueued for processing instead of being processed immediately when Publish is called.
+//The background task will process the messages from the queue and dispatch them to the appropriate consumers.
+//await bus.Publish(new OrderSubmitted
+//{
+//    OrderId = Guid.NewGuid()
+//});
+
+//Console.WriteLine("Done. Press any key...");
+//Console.ReadKey();
