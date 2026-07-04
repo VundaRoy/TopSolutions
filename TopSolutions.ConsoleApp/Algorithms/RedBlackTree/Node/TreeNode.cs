f48df232620d54aa7c6a1c97d9c01745644f9dda
Roy Chang <roychang@live.com.au>
2026-07-04 20:37:39 +1000

    SoC implementation and cleanup

---
+++ TopSolutions.ConsoleApp/Algorithms/RedBlackTree/Node/TreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSolutions.ConsoleApp.Algorithms.RedBlackTree.Node
+{
+    //Red-black tree node class with color property and left, right, and parent pointers
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+        public TreeNode Parent { get; set; }
+        public bool IsRed { get; set; } // True for red, false for black
+        public TreeNode(int value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+            Parent = null;
+            IsRed = true; // New nodes are red by default
+        }
+    }
+}
---
+++ TopSolutions.ConsoleApp/Algorithms/RedBlackTree/Operations/Rotation.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TopSolutions.ConsoleApp.Algorithms.RedBlackTree.Node;
+
+namespace TopSolutions.ConsoleApp.Algorithms.RedBlackTree.Operations
+{
+    internal class Rotation
+    {
+        // Implement left and right rotation methods for the Red-Black Tree
+        public static TreeNode LeftRotate(TreeNode root, TreeNode x)
+        {
+            TreeNode y = x.Right;
+            x.Right = y.Left;
+            if (y.Left != null)
+            {
+                y.Left.Parent = x;
+            }
+            y.Parent = x.Parent;
+            if (x.Parent == null)
+            {
+                root = y;
+            }
+            else if (x == x.Parent.Left)
+            {
+                x.Parent.Left = y;
+            }
+            else
+            {
+                x.Parent.Right = y;
+            }
+            y.Left = x;
+            x.Parent = y;
+            return root;
+        }
+        //right rotation method
+        public static TreeNode RightRotate(TreeNode root, TreeNode y)
+        {
+            TreeNode x = y.Left;
+            y.Left = x.Right;
+            if (x.Right != null)
+            {
+                x.Right.Parent = y;
+            }
+            x.Parent = y.Parent;
+            if (y.Parent == null)
+            {
+                root = x;
+            }
+            else if (y == y.Parent.Right)
+            {
+                y.Parent.Right = x;
+            }
+            else
+            {
+                y.Parent.Left = x;
+            }
+            x.Right = y;
+            y.Parent = x;
+            return root;
+        }
+    }
+}
