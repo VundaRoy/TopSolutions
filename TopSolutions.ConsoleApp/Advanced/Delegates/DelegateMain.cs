859e273f9c22a815d8812d3cfe148589649b7abf
VundaRoy <roychang@live.com.au>
2026-07-21 10:54:56 +1000

    simple delegate

---
+++ TopSolutions.ConsoleApp/Advanced/Delegates/DelegateMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopSolutions.ConsoleApp.Advanced.Delegates
+{
+    internal class DelegateMain
+    {
+        //Delegate sample
+        delegate void Calculate(int a, int b);
+        public static void Main()
+        {
+            //lambda expression for delegate
+            Calculate add = (a, b) => Console.WriteLine($"Addition: {a + b}");
+            //subtract method for delegate
+            Calculate minus = (a, b) => Console.WriteLine($"Subtraction: {a - b}");
+            //divide method for delegate
+            Calculate divide = (a, b) => Console.WriteLine($"Division: {a / b}");
+            //multiply method for delegate
+            Calculate multiply = (a, b) => Console.WriteLine($"Multiplication: {a * b}");
+            //running the delegate methods
+            add(10, 5);
+            minus(10, 5);
+            divide(10, 5);
+            multiply(10, 5);
+        }
+    }
+}
