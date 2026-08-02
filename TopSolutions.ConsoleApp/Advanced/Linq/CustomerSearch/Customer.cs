ddcaf57428aea4e471b5ee13100e04ade5867d7a
VundaRoy <roychang@live.com.au>
2026-08-03 09:30:42 +1000

    Simple linq search

---
+++ TopSolutions.ConsoleApp/Advanced/Linq/CustomerSearch/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopSolutions.ConsoleApp.Advanced.Linq.CustomerSearch
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+    }
+}
---
+++ TopSolutions.ConsoleApp/Advanced/Linq/CustomerSearch/CustomerMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopSolutions.ConsoleApp.Advanced.Linq.CustomerSearch
+{
+    internal class CustomerMain
+    {
+        //List of customers to search from
+        public static List<Customer> CustomerList;
+        //a program to utilise Linq to look up customer based on id
+        public static void InititaliseData()
+        {
+            //create data for CustomerList
+            CustomerList = new List<Customer>() {
+                new Customer { CustomerId = "C001", FirstName = "John", LastName = "Doe", Address = "123 Main St" },
+                new Customer { CustomerId = "C002", FirstName =  "Jane", LastName = "Smith", Address = "456 Elm St" },
+            new Customer{CustomerId = "C003", FirstName = "Coleman", LastName = "Johnson", Address = "789 Oak St" }};
+
+        }
+        public static void Main(string[] args)
+        {
+            //linq to search by customer id
+            InititaliseData();
+            //Get first name from CustomerList using linq where customer id is C001
+            var customer = CustomerList.Where(c => c.CustomerId == "C001").FirstOrDefault();
+            if (customer != null)
+            {
+                Console.WriteLine($"Customer found: {customer.FirstName} {customer.LastName}, Address: {customer.Address}");
+            }
+            else
+            {
+                Console.WriteLine("Customer not found.");
+            }
+        }
+    }
+}
