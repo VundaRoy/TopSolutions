65cbf3135b6a6c6d0d99e3791ceba869d4e446bf
VundaRoy <roychang@live.com.au>
2026-08-06 09:27:13 +1000

    Reflection sample

---
+++ TopSolutions.ConsoleApp/Advanced/Reflection/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopSolutions.ConsoleApp.Advanced.Reflection
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int NumberOfOrders { get; set; }
+        public decimal TotalCharged { get; set; }
+    }
+}
---
+++ TopSolutions.ConsoleApp/Advanced/Reflection/ReflectionMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopSolutions.ConsoleApp.Advanced.Reflection
+{
+    internal class ReflectionMain
+    {
+        public static void Main(string[] args)
+        {
+            // Get the type of the Customer class
+            Type customerType = typeof(Customer);
+            // Get the properties of the Customer class
+            var properties = customerType.GetProperties();
+            Console.WriteLine("Properties of Customer class:");
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"- {property.Name} ({property.PropertyType.Name})");
+            }
+            // Create an instance of the Customer class using reflection
+            var customerInstance = Activator.CreateInstance(customerType);
+            // Set property values using reflection
+            customerType.GetProperty("CustomerId")?.SetValue(customerInstance, "C007");
+            customerType.GetProperty("FirstName")?.SetValue(customerInstance, "David");
+            customerType.GetProperty("LastName")?.SetValue(customerInstance, "Miller");
+            customerType.GetProperty("Address")?.SetValue(customerInstance, "404 Cedar St");
+            customerType.GetProperty("NumberOfOrders")?.SetValue(customerInstance, 7);
+            customerType.GetProperty("TotalCharged")?.SetValue(customerInstance, 350.75m);
+            // Get property values using reflection
+            Console.WriteLine("\nCustomer instance created using reflection:");
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(customerInstance);
+                Console.WriteLine($"- {property.Name}: {value}");
+            }
+        }
+    }
+}
