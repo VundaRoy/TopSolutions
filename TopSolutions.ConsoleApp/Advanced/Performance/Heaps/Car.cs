127f7e835686ee4982114add8d74b8b0d91caebc
VundaRoy <roychang@live.com.au>
2026-08-14 10:35:42 +1000

    Heaps and stack sample

---
+++ TopSolutions.ConsoleApp/Advanced/Performance/Heaps/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopSolutions.ConsoleApp.Advanced.Performance.Heaps
+{
+    //A car object to be used as an example for a heap data structure
+    public class Car
+    {
+        public int Price { get; set; }
+        public string Model { get; set; }
+        public string Maker { get; set; }
+        public Car(string model, string maker, int price) 
+        {
+            Model = model;
+            Maker = maker;
+            Price = price;
+        }       
+
+    }
+}
---
+++ TopSolutions.ConsoleApp/Advanced/Performance/Heaps/HeapsMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopSolutions.ConsoleApp.Advanced.Performance.Heaps
+{
+    class HeapsMain
+    {
+        public static void Main(string[] args)
+        {
+            //Create a car as a heap example
+            Car car = new Car("Model S", "Tesla", 80000); // Create a car object with model, maker, and price
+            Console.WriteLine(car);
+            Console.WriteLine($"The maker is {car.Maker}");
+            //Struct example
+            NumberOfDoorsAndWheels numberOfDoorsAndWheels = new NumberOfDoorsAndWheels(4, 4); // Create a struct object with number of doors and wheels
+            Console.WriteLine(numberOfDoorsAndWheels);
+            Console.WriteLine($"The number of doors is {numberOfDoorsAndWheels.NumberOfDoors}");
+        }
+        struct NumberOfDoorsAndWheels
+        {
+            public int NumberOfDoors { get; set; }
+            public int NumberOfWheels { get; set; }
+            public NumberOfDoorsAndWheels(int numberOfDoors, int numberOfWheels)
+            {
+                NumberOfDoors = numberOfDoors;
+                NumberOfWheels = numberOfWheels;
+            }
+        }
+    }
+}
