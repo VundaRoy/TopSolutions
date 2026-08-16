23231a52186039030c988f25131683b3f528a6a9
VundaRoy <roychang@live.com.au>
2026-08-17 09:04:52 +1000

    Simple span example with integers

---
+++ TopSolutions.ConsoleApp/Advanced/Performance/Span/SpanMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopSolutions.ConsoleApp.Advanced.Performance.Span
+{
+    public class SpanMain
+    {
+        //Span usgage example
+        public SpanMain() { }  
+        public void SpanExample()
+        {
+            //Create a span of integers
+            Span<int> numbers = [1, 2, 3, 4, 5]; // Create a span of integers with 5 elements
+            Console.WriteLine("Span of integers:");
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            //Create an instance of SpanMain and call the SpanExample method
+            SpanMain spanMain = new SpanMain();
+            spanMain.SpanExample();
+
+        }
+    }
+}
