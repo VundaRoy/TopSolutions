d47f977aed1a5d8f5f4e028134ed1d1f28679a13
VundaRoy <roychang@live.com.au>
2026-08-18 09:45:21 +1000

    span of type car and slicing

---
+++ TopSolutions.ConsoleApp/Advanced/Performance/Span/TypeOf/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopSolutions.ConsoleApp.Advanced.Performance.Span.TypeOf
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Maker { get; set; }
+    }
+}
---
+++ TopSolutions.ConsoleApp/Advanced/Performance/Span/TypeOf/SliceOfCarMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopSolutions.ConsoleApp.Advanced.Performance.Span.TypeOf
+{
+    public class SliceOfCarMain
+    {
+        public static void Main(string[] args)
+        {
+            //Create a span of Car objects
+            Span<Car> cars = new Car[]
+            {
+                new Car { Model = "Model S", Name = "Tesla", Price = 80000, Maker = "Tesla" },
+                new Car { Model = "Mustang", Name = "Ford", Price = 55000, Maker = "Ford" },
+                new Car { Model = "Civic", Name = "Honda", Price = 25000, Maker = "Honda" },
+                new Car { Model = "Camry", Name = "Toyota", Price = 30000, Maker = "Toyota" },
+                new Car { Model = "Accord", Name = "Honda", Price = 28000, Maker = "Honda" }
+            };
+            Console.WriteLine("Span of Car objects:");
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"Model: {car.Model}, Name: {car.Name}, Price: {car.Price}, Maker: {car.Maker}");
+            }
+            //Slice the span to get the first 3 elements
+            Span<Car> slicedCars = cars.Slice(0, 3);
+            Console.WriteLine("Sliced span of Car objects:");
+            foreach (var car in slicedCars)
+            {
+                Console.WriteLine($"Model: {car.Model}, Name: {car.Name}, Price: {car.Price}, Maker: {car.Maker}");
+            }
+        }
+    }
+}
