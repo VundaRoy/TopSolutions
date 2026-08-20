d831bf58bdf68d9f33493b2b41f94246d76f2755
VundaRoy <roychang@live.com.au>
2026-08-20 10:41:11 +1000

    refactored new for slice

--- TopSolutions.ConsoleApp/Advanced/Performance/Span/TypeOf/SliceOfCarMain.cs
+++ TopSolutions.ConsoleApp/Advanced/Performance/Span/TypeOf/SliceOfCarMain.cs
@@ -11,11 +11,11 @@ namespace TopSolutions.ConsoleApp.Advanced.Performance.Span.TypeOf
             //Create a span of Car objects
             Span<Car> cars = new Car[]
             {
-                new Car { Model = "Model S", Name = "Tesla", Price = 80000, Maker = "Tesla" },
-                new Car { Model = "Mustang", Name = "Ford", Price = 55000, Maker = "Ford" },
-                new Car { Model = "Civic", Name = "Honda", Price = 25000, Maker = "Honda" },
-                new Car { Model = "Camry", Name = "Toyota", Price = 30000, Maker = "Toyota" },
-                new Car { Model = "Accord", Name = "Honda", Price = 28000, Maker = "Honda" }
+                new () { Model = "Model S", Name = "Tesla", Price = 80000, Maker = "Tesla" },
+                new () { Model = "Mustang", Name = "Ford", Price = 55000, Maker = "Ford" },
+                new () { Model = "Civic", Name = "Honda", Price = 25000, Maker = "Honda" },
+                new () { Model = "Camry", Name = "Toyota", Price = 30000, Maker = "Toyota" },
+                new () { Model = "Accord", Name = "Honda", Price = 28000, Maker = "Honda" }
             };
             Console.WriteLine("Span of Car objects:");
             foreach (var car in cars)
