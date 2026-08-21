4819764a171fc5401bd3f205d30e2f2faddae873
VundaRoy <roychang@live.com.au>
2026-08-21 10:33:41 +1000

    Get middle elements with slice

--- TopSolutions.ConsoleApp/Advanced/Performance/Span/TypeOf/SliceOfCarMain.cs
+++ TopSolutions.ConsoleApp/Advanced/Performance/Span/TypeOf/SliceOfCarMain.cs
@@ -16,7 +16,11 @@ namespace TopSolutions.ConsoleApp.Advanced.Performance.Span.TypeOf
                 new () { Model = "Civic", Name = "Honda", Price = 25000, Maker = "Honda" },
                 new () { Model = "Camry", Name = "Toyota", Price = 30000, Maker = "Toyota" },
                 new () { Model = "Accord", Name = "Honda", Price = 28000, Maker = "Honda" },
-                new () { Model = "Corolla", Name = "Toyota", Price = 20000, Maker = "Toyota" }
+                new () { Model = "Corolla", Name = "Toyota", Price = 20000, Maker = "Toyota" },
+                new () { Model = "Model 3", Name = "Tesla", Price = 40000, Maker = "Tesla" },
+                new () { Model = "F-150", Name = "Ford", Price = 50000, Maker = "Ford" },
+                new () { Model = "Altima", Name = "Nissan", Price = 27000, Maker = "Nissan" },
+                new () { Model = "Sentra", Name = "Nissan", Price = 22000, Maker = "Nissan" }
             };
             Console.WriteLine("Span of Car objects:");
             foreach (var car in cars)
@@ -38,6 +42,23 @@ namespace TopSolutions.ConsoleApp.Advanced.Performance.Span.TypeOf
             {
                 Console.WriteLine($"Model: {car.Model}, Name: {car.Name}, Price: {car.Price}, Maker: {car.Maker}");
 
+            }
+            //Other slice uses
+            //slice example to get the middle 3 elements
+            Console.WriteLine("Getting the middle 3 elements of the span:");
+            slicedCars.Clear();
+            slicedCars = cars.Slice(1, 3);
+            foreach (var car in slicedCars)
+            {
+                //check if not null
+                if (car != null)
+                {
+                    Console.WriteLine($"Model: {car.Model}, Name: {car.Name}, Price: {car.Price}, Maker: {car.Maker}");
+                }
+                else
+                {
+                    Console.WriteLine("the car is null"); 
+                }
             }
         }
     }
