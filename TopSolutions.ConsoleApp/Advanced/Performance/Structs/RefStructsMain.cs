1851ea0ee35af95bce9de93242a0e30c814832c5
VundaRoy <roychang@live.com.au>
2026-08-27 09:44:17 +1000

    struct memory sample

---
+++ TopSolutions.ConsoleApp/Advanced/Performance/Structs/RefStructsMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopSolutions.ConsoleApp.Advanced.Performance.Structs
+{
+    internal class RefStructsMain
+    {
+        public static void Main(string[] args)
+        {
+            //create a stack only struct
+            StackOnlyStruct stackOnlyStruct = new StackOnlyStruct(10);
+            Console.WriteLine($"Initial Value: {stackOnlyStruct.GetAmount()}");
+            stackOnlyStruct.AddAmount(5);
+            Console.WriteLine($"After Adding 5: {stackOnlyStruct.GetAmount()}");
+            stackOnlyStruct.RemoveAmount(3);
+            Console.WriteLine($"After Removing 3: {stackOnlyStruct.GetAmount()}");
+
+        }
+        //create stack only structures
+        //struct of integers that can only be allocated on the stack and cannot be boxed or stored in heap memory
+        public struct StackOnlyStruct
+        {
+            public int Value;
+            public StackOnlyStruct(int value)
+            {
+                Value = value;
+            }
+            public void AddAmount(int amount)
+            {
+                Value += amount;
+            }
+            public int GetAmount()
+            {
+                return Value;
+            }
+            public void RemoveAmount(int amount){
+                {
+                    Value -= amount;
+                }
+
+            }
+        }
+        
+        
+    }
+}
