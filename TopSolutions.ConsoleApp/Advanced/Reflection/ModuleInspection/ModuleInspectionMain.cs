b9f182fa9ab241897d53e61fb90a0e836c94c36d
VundaRoy <roychang@live.com.au>
2026-08-31 13:45:22 +1000

    module  inspection

---
+++ TopSolutions.ConsoleApp/Advanced/Reflection/ModuleInspection/ModuleInspectionMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopSolutions.ConsoleApp.Advanced.Reflection.ModuleInspection
+{
+    internal class ModuleInspectionMain
+    {
+        public static void Main(string[] args)
+        {
+            // Get the current assembly
+            var assembly = typeof(ModuleInspectionMain).Assembly;
+            // Get all modules in the assembly
+            var modules = assembly.GetModules();
+            Console.WriteLine("Modules in the current assembly:");
+            foreach (var module in modules)
+            {
+                Console.WriteLine($"- {module.Name}");
+            }
+        }
+    }
+}
